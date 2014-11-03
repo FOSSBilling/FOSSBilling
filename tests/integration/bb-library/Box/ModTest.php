7ce4355fdc4a83856f858c568ee6cbc2b4984f24
4Giedrius <4giedrius@gmail.com>
2014-11-03 15:08:23 +0200

    Integration tests updated

--- tests/integration/bb-library/Box/ModTest.php
+++ tests/integration/bb-library/Box/ModTest.php
@@ -5,16 +5,16 @@
  */
 class Box_ModIntegrationTest extends BBDbApiTestCase
 {
-    public function testModel()
+    public function testConfig()
     {
         $conf = array(
-            'last_name'
+            'ext'      => 'mod_client',
+            'required' => array(
+                'last-name'
+            )
         );
 
-        $this->api_admin->extension_config_save(array(
-            'ext'      => 'mod_client',
-            'required' => $conf
-        ));
+        $this->api_admin->extension_config_save($conf);
 
         $config = $this->di['mod_config']('client');
         $this->assertEquals($conf, $config);
