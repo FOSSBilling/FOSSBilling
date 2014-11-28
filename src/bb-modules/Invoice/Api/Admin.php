af51224e61b3729fc5ee507ec31ad26108ddecb1
4Giedrius <4giedrius@gmail.com>
2014-11-28 15:55:22 +0200

    Batch delete implemented for Invoices list

--- src/bb-modules/Invoice/Api/Admin.php
+++ src/bb-modules/Invoice/Api/Admin.php
@@ -884,4 +884,25 @@ class Admin extends \Api_Abstract
 
         return $model;
     }
+
+    /**
+     * Deletes invoices with given IDs
+     *
+     * @param array $ids - IDs for deletion
+     *
+     * @return bool
+     */
+    public function batch_delete($data)
+    {
+        $required = array(
+            'ids' => 'IDs not passed',
+        );
+        $this->di['validator']->checkRequiredParamsForArray($required, $data);
+
+        foreach ($data['ids'] as $id) {
+            $this->delete(array('id' => $id));
+        }
+
+        return true;
+    }
 }
