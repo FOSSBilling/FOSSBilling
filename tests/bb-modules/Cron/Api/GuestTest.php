ec79bbeb5adbb8566fb24885d732683ca6e7d12b
4Giedrius <4giedrius@gmail.com>
2015-01-09 16:25:02 +0200

    Web cron removed

--- tests/bb-modules/Cron/Api/GuestTest.php
+++ tests/bb-modules/Cron/Api/GuestTest.php
@@ -17,49 +17,11 @@ class GuestTest extends \PHPUnit_Framework_TestCase {
 
     public function testcheck()
     {
-        $configArr = array(
-            'use_web_cron' => true,
-        );
-
-        $serviceMock = $this->getMockBuilder('\Box\Mod\Cron\Service')->getMock();
-        $serviceMock->expects($this->atLeastOnce())->method('isLate')->will($this->returnValue(false));
-
-        $modMock = $this->getMockBuilder('\Box_Mod')->disableOriginalConstructor()->getMock();
-        $modMock->expects($this->atLeastOnce())->method('getConfig')->will($this->returnValue($configArr));
-
-        $di = new \Box_Di();
-//        $di['mod_config'] = $di->protect(function ($name) use($configArr) { return $configArr;  });
-
         $api = new \Box\Mod\Cron\Api\Guest();
-        $api->setDi($di);
-        $api->setService($serviceMock);
-        $api->setMod($modMock);
 
         $result = $api->check();
         $this->assertInternalType('bool', $result);
         $this->assertFalse($result);
-    }
-
-    public function testcheckTrue()
-    {
-        $configArr = array(
-            'use_web_cron' => true,
-        );
-
-        $serviceMock = $this->getMockBuilder('\Box\Mod\Cron\Service')->getMock();
-        $serviceMock->expects($this->atLeastOnce())->method('isLate')->will($this->returnValue(true));
-        $serviceMock->expects($this->atLeastOnce())->method('runCrons');
-
-        $modMock = $this->getMockBuilder('\Box_Mod')->disableOriginalConstructor()->getMock();
-        $modMock->expects($this->atLeastOnce())->method('getConfig')->will($this->returnValue($configArr));
-
-        $api = new \Box\Mod\Cron\Api\Guest();
-        $api->setService($serviceMock);
-        $api->setMod($modMock);
-
-        $result = $api->check();
-        $this->assertInternalType('bool', $result);
-        $this->assertTrue($result);
     }
 
     public  function testsettings()
