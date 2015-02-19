3ebaf324d72ec604223c9a0f57a24765bed35418
4Giedrius <4giedrius@gmail.com>
2015-02-19 10:45:24 +0200

    Added getEuVat() method which returns standart VAT rate for each EU country

--- src/bb-modules/System/Service.php
+++ src/bb-modules/System/Service.php
@@ -668,6 +668,40 @@ class Service
         return $res;
     }
 
+    public function getEuVat()
+    {
+        return array(
+            'AT' => 20, //Austria
+            'BE' => 21, //Belgium
+            'BG' => 20, //Bulgaria
+            'HR' => 25, //Croatia
+            'CY' => 19, //Cyprus
+            'CZ' => 21, //Czech Republic
+            'DK' => 25, //Denmark
+            'EE' => 20, //Estonia
+            'FI' => 24, //Finland
+            'FR' => 20, //France
+            'DE' => 19, //Germany
+            'GR' => 23, //Greece
+            'HU' => 27, //Hungary
+            'IE' => 23, //Ireland
+            'IT' => 22, //Italy
+            'LV' => 21, //Latvia
+            'LT' => 21, //Lithuania
+            'LU' => 15, //Luxembourg
+            'MT' => 18, //Malta
+            'NL' => 21, //Netherlands
+            'PL' => 23, //Poland
+            'PT' => 23, //Portugal
+            'RO' => 24, //Romania
+            'SK' => 20, //Slovakia
+            'SI' => 22, //Slovenia
+            'ES' => 21, //Spain
+            'SE' => 25, //Sweden
+            'GB' => 20, //United Kingdom
+        );
+    }
+
     public function getStates()
     {
         return array (
