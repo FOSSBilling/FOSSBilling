0d482fd9a4c91def4788d7977b5b128f18808e1d
Vaidas Lungis <vaidas.lungis+github@gmail.com>
2015-03-03 16:14:54 +0200

    add __call method to Server_Client and Server_Package
    return empty string when inaccessible method is called
    error log is created to debug it.

--- src/bb-library/Server/Package.php
+++ src/bb-library/Server/Package.php
@@ -30,6 +30,19 @@ class Server_Package
     private $maxpop                 = NULL;
     
     private $custom                 = array();
+
+    public function __call($name, $arguments)
+    {
+        if (version_compare(PHP_VERSION, '5.4.0') < 0) {
+            $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        }
+        else {
+            // Get only the stack frames we need (PHP 5.4 only).
+            $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
+        }
+        error_log(sprintf("Calling %s inaccessible method %s from %s::%d", get_class($this), $name, $backtrace[1]['file'], $backtrace[1]['line']));
+        return '';
+    }
     
     public function setCustomValues(array $param)
     {
