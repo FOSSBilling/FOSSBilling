4ff3c2560ac2622e72326c1f4251cf7b805ff283
Vaidas Lungis <vaidas.lungis+github@gmail.com>
2015-05-21 14:18:39 +0300

    use sprite in left navigation bar

--- src/bb-modules/Activity/Controller/Admin.php
+++ src/bb-modules/Activity/Controller/Admin.php
@@ -41,6 +41,7 @@ class Admin implements \Box\InjectionAwareInterface
                 'location'  =>  'activity',
                 'label' => 'Activity',
                 'class'     => 'graphs',
+                'sprite_class' => 'sprite sprite-graph',
                 ),
             'subpages'=> array(
                 array(
--- src/bb-modules/Product/Controller/Admin.php
+++ src/bb-modules/Product/Controller/Admin.php
@@ -42,6 +42,7 @@ class Admin implements \Box\InjectionAwareInterface
                 'label' => 'Products',
                 'uri' => $this->di['url']->adminLink('products'),
                 'class' => 'pic',
+                'sprite_class' => 'sprite sprite-blocks',
             ),
             'subpages'  =>  array(
                 array(
