a1dc1176411b78c988d59cee538702eb838bf90c
Mr. Timothy Glenn Webb Sr <timothy@webbhostingservices.com>
2020-10-12 18:02:51 +0000

    Plesk Refactor 3

--- src/bb-library/Server/Manager/Plesk.php
+++ src/bb-library/Server/Manager/Plesk.php
@@ -1,12 +1,15 @@
 <?php
-#!/usr/bin/env php
 // Copyright 1999-2016. Parallels IP Holdings GmbH. All Rights Reserved.
+
 require_once('PleskApiClient.php');
+
 $host = getenv('REMOTE_HOST');
 $login = getenv('REMOTE_LOGIN') ?: 'admin';
 $password = getenv('REMOTE_PASSWORD');
+
 $client = new Server_Manager_Plesk($host);
 $client->setCredentials($login, $password);
+
 $request = <<<EOF
 <packet>
   <server>
@@ -14,6 +17,6 @@ $request = <<<EOF
   </server>
 </packet>
 EOF;
+
 $response = $client->request($request);
 echo $response;
-?>
--- src/bb-library/Server/Manager/PleskApiClient.php
+++ src/bb-library/Server/Manager/PleskApiClient.php
@@ -1,29 +1,10 @@
 <?php
+// Copyright 1999-2016. Parallels IP Holdings GmbH. All Rights Reserved.
+
 /**
- * BoxBilling
- *
- * LICENSE
- *
- * This source file is subject to the license that is bundled
- * with this package in the file LICENSE.txt
- * It is also available through the world-wide-web at this URL:
- * http://www.boxbilling.com/LICENSE.txt
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@boxbilling.com so we can send you a copy immediately.
- *
- * @copyright Copyright (c) 2010-2012 BoxBilling (http://www.boxbilling.com)
- * @license   http://www.boxbilling.com/LICENSE.txt
- * @version   $Id$
+ * Client for Plesk API-RPC
  */
-/**
- *
- * Plesk server manager
- * Min version: 12.0
- * @see http://download1.parallels.com/Plesk/Doc/en-US/online/plesk-api-rpc/
- *
- */
-class Server_Manager_Plesk
+class Server_Manager_Plesk extends Server_Manager
 {
 	public function init() {
         if (!extension_loaded('curl')) {
@@ -52,70 +33,7 @@ class Server_Manager_Plesk
     {
         return $this->getLoginUrl();
     }
-    /**
-     * Setup credentials for authentication
-     *
-     * @param string $login
-     * @param string $password
-     */
-    public function setCredentials($login, $password)
-    {
-        $this->_login = $login;
-        $this->_password = $password;
-    }
-	public function setSecretKey($secretKey)
-    {
-        $this->_secretKey = $secretKey;
-    }
-
-    /**
-     * Perform API request
-     *
-     * @param string $request
-     * @return string
-     */
-    public function request($request)
-    {
-        $curl = curl_init();
-
-        curl_setopt($curl, CURLOPT_URL, '$this->$protocol://$this->_host:$this->_port/enterprise/control/agent.php');
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->_getHeaders());
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
-
-        $result = curl_exec($curl);
-
-        curl_close($curl);
-
-        return $result;
-    }
-
-    /**
-     * Retrieve list of headers needed for request
-     *
-     * @return array
-     */
-    private function _getHeaders()
-    {
-        $headers = array(
-            "Content-Type: text/xml",
-            "HTTP_PRETTY_PRINT: TRUE",
-        );
-
-     {
-           
-            $headers[] = "HTTP_AUTH_LOGIN: $this->_login";
-            $headers[] = "HTTP_AUTH_PASSWD: $this->_password";
-        }
-
-        return $headers;
-    }
-
-
-    public function testConnection()
+	public function testConnection()
     {
     	$params = array (
     		'server'	=>	array(
@@ -1024,4 +942,94 @@ class Server_Manager_Plesk
    			return false;
    		}
     }
+    private $_host;
+    private $_port;
+    private $_protocol;
+    private $_login;
+    private $_password;
+    private $_secretKey;
+
+    /**
+     * Create client
+     *
+     * @param string $host
+     * @param int $port
+     * @param string $protocol
+     */
+    public function __construct($host, $port = 8443, $protocol = 'https')
+    {
+        $this->_host = $host;
+        $this->_port = $port;
+        $this->_protocol = $protocol;
+    }
+
+    /**
+     * Setup credentials for authentication
+     *
+     * @param string $login
+     * @param string $password
+     */
+    public function setCredentials($login, $password)
+    {
+        $this->_login = $login;
+        $this->_password = $password;
+    }
+
+    /**
+     * Define secret key for alternative authentication
+     *
+     * @param string $secretKey
+     */
+    public function setSecretKey($secretKey)
+    {
+        $this->_secretKey = $secretKey;
+    }
+
+    /**
+     * Perform API request
+     *
+     * @param string $request
+     * @return string
+     */
+    public function request($request)
+    {
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, "$this->_protocol://$this->_host:$this->_port/enterprise/control/agent.php");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->_getHeaders());
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $result;
+    }
+
+    /**
+     * Retrieve list of headers needed for request
+     *
+     * @return array
+     */
+    private function _getHeaders()
+    {
+        $headers = array(
+            "Content-Type: text/xml",
+            "HTTP_PRETTY_PRINT: TRUE",
+        );
+
+        if ($this->_secretKey) {
+            $headers[] = "KEY: $this->_secretKey";
+        } else {
+            $headers[] = "HTTP_AUTH_LOGIN: $this->_login";
+            $headers[] = "HTTP_AUTH_PASSWD: $this->_password";
+        }
+
+        return $headers;
+    }
+
 }
