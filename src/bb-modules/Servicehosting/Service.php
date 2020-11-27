ece87132754bf19700f5da497f43dc705e1703b9
Mr. Timothy G Webb Sr <webb.timothy@gmail.com>
2020-11-27 01:29:27 -0500

    Update Service.php

--- src/bb-modules/Servicehosting/Service.php
+++ src/bb-modules/Servicehosting/Service.php
@@ -444,9 +444,9 @@ class Service implements InjectionAwareInterface
 
     public function toApiArray(\Model_ServiceHosting $model, $deep = false, $identity = null)
     {
-        $serviceHostingModel = $this->di['db']->load('ServiceHostingServer', $model->service_hosting_server_id);
+        $serviceHostingServerModel = $this->di['db']->load('ServiceHostingServer', $model->service_hosting_server_id);
         $serviceHostingHpModel = $this->di['db']->load('ServiceHostingHp', $model->service_hosting_hp_id);
-        $server = $this->toHostingServerApiArray($serviceHostingModel, $deep, $identity);
+        $server = $this->toHostingServerApiArray($serviceHostingServerModel, $deep, $identity);
         $hp = $this->toHostingHpApiArray($serviceHostingHpModel, $deep, $identity);
 
         return array(
