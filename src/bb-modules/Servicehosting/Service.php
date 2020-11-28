307c16cebf8301cf3891cb63d1fe4f7007651d58
Mr. Timothy G Webb Sr <timothy@webbhostingservices.com>
2020-11-27 23:46:47 -0500

    Update Service.php

--- src/bb-modules/Servicehosting/Service.php
+++ src/bb-modules/Servicehosting/Service.php
@@ -100,7 +100,7 @@ class Service implements InjectionAwareInterface
         $orderService = $this->di['mod_service']('order');
         $model = $orderService->getOrderService($order);
         if(!$model instanceof \RedBean_SimpleModel) {
-            throw new \Box_Exception('Could not activate order. Service was not created');
+            throw new \Box_Exception('Order :id has no active service', array(':id'=>$order->id));
         }
 
         $pass = $this->di['tools']->generatePassword(10, 4);
