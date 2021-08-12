37df3e503782143723ab272b330f6826c8d2a4dd
evrifaessa <yagizhan49@protonmail.com>
2021-08-12 14:18:31 +0300

    Updated user agent

--- src/bb-library/Box/Tools.php
+++ src/bb-library/Box/Tools.php
@@ -63,7 +63,7 @@ class Box_Tools
     public function get_url($url, $timeout = 10)
     {
         $ch = curl_init();
-        $userAgent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)';
+        $userAgent = 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0';
         curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
