f3bbf9c1fb79a27efc7b9f53b921549164538fba
Yağızhan Burak Yakar <yagizhan49@protonmail.com>
2022-05-18 19:15:57 +0300

    Minor rebranding

--- src/bb-load.php
+++ src/bb-load.php
@@ -214,8 +214,8 @@ function handler_exception($e)
             }
             echo sprintf('<p>%s</p>', $e->getMessage());
 
-            echo sprintf('<center><p><a href="http://docs.boxbilling.com/en/latest/search.html?q=%s&check_keywords=yes&area=default" target="_blank">Look for detailed error explanation</a></p></center>', urlencode($e->getMessage()));
-            echo '<center><hr><p>Powered by <a href="https://github.com/boxbilling/boxbilling/">BoxBilling</a></p></center>
+            echo sprintf('<center><p><a href="https://docs.fossbilling.org/en/latest/search.html?q=%s&check_keywords=yes&area=default" target="_blank">Look for detailed error explanation</a></p></center>', urlencode($e->getMessage()));
+            echo '<center><hr><p>Powered by <a href="https://fossbilling.org">FOSSBilling</a></p></center>
       </body>
       
       </html>';
