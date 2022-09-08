9fa5d9933b93a230799a02be25ae8064d0a81e16
kenny <kenny@kenny-it.nl>
2022-09-08 06:02:28 +0200

    Fixed admin typo in table. Updated auth Added some Components

--- src/app/Console/Commands/SetUp.php
+++ src/app/Console/Commands/SetUp.php
@@ -47,122 +47,121 @@ class SetUp extends Command
             function () {
                 $this->call('migrate');
 
-                        // Model User
-        $this->askStep(
-            'Add Super Admin User',
-             function () {
-                 $user = User::create(
-                 [
-                     'id' => 5,
-                     'name' => 'Admin',
-                     'email' => 'admin@localhost',
-                     'password' => Hash::make('password')
-                 ]);
-             }
-         );
-         //Create the roles now so we can use the min the migration
-         $superadmin = Role::firstOrCreate(['name' => 'Super Admin']);
-         if (isset($user)){
-             $user = User::first();
-             $user->assignRole($superadmin);
-         }
- 
-         $admin = Role::firstOrCreate(['name' => 'admin']);
-         $permission = Permission::firstOrCreate(['name' => 'view admin']);
-         $admin->givePermissionTo($permission);
-         $permission = Permission::firstOrCreate(['name' => 'edit settings']);
-         $admin->givePermissionTo($permission);
- 
-         $staff = Role::firstOrCreate(['name' => 'staff']);
-        
-         $this->info("Created roles");
+                // Model User
+                $this->askStep(
+                    'Add Super Admin User',
+                    function () {
+                        $user = Admin::create(
+                            [
+                                'name' => 'Admin',
+                                'email' => 'admin@localhost',
+                                'password' => Hash::make('password')
+                            ]);
+                        $superadmin = Role::firstOrCreate(['name' => 'Super Admin', "guard_name" => "admin"]);
+                        $user->assignRole([$superadmin]);
+                        $user->save();
+
+                    }
+                );
+                //Create the roles now so we can use the min the migration
+
+
+                $admin = Role::firstOrCreate(['name' => 'admin', "guard_name" => "admin"]);
+                $permission = Permission::firstOrCreate(['name' => 'view admin', "guard_name" => "admin"]);
+                $admin->givePermissionTo($permission);
+                $permission = Permission::firstOrCreate(['name' => 'edit settings', "guard_name" => "admin"]);
+                $admin->givePermissionTo($permission);
+
+                $staff = Role::firstOrCreate(['name' => 'staff', "guard_name" => "admin"]);
+
+                $this->info("Created roles");
                 //settings table
                 if (Schema::hasTable('setting')) {
 
                     DB::table('setting')
-                    ->lazyById()->each(function ($setting) {
-                        $row = Setting::firstOrCreate(['key'=>$setting->param],[
-                            'key'=>$setting->param,
-                            'value'=>$setting->value
-                        ]);
-                        $row->save();
-                    });
+                        ->lazyById()->each(function ($setting) {
+                            $row = Setting::firstOrCreate(['key' => $setting->param], [
+                                'key' => $setting->param,
+                                'value' => $setting->value
+                            ]);
+                            $row->save();
+                        });
                     Schema::drop('setting');
                 }
                 //currency table
                 if (Schema::hasTable('currency')) {
 
                     DB::table('currency')
-                    ->lazyById()->each(function ($currency) {
-                        $row = Currency::firstOrCreate(['code'=>$currency->code],[
-                            'title'=>$currency->title,
-                            'code'=>$currency->code,
-                            'is_default'=>$currency->is_default,
-                            'conversion_rate'=>$currency->conversion_rate
-                        ]);
-                        $row->save();
-                    });
+                        ->lazyById()->each(function ($currency) {
+                            $row = Currency::firstOrCreate(['code' => $currency->code], [
+                                'title' => $currency->title,
+                                'code' => $currency->code,
+                                'is_default' => $currency->is_default,
+                                'conversion_rate' => $currency->conversion_rate
+                            ]);
+                            $row->save();
+                        });
                     Schema::drop('currency');
                 }
                 //tax table
                 if (Schema::hasTable('tax')) {
 
                     DB::table('tax')
-                    ->lazyById()->each(function ($tax) {
-                        $row = Tax::firstOrCreate(['level'=>$tax->level,'country'=>$tax->country,'state'=>$tax->state],[
-                            'level'=>($tax->level === null ? 0: $tax->level),
-                            'name'=>$tax->name,
-                            'country'=>$tax->country,
-                            'state'=>$tax->state,
-                            'taxrate'=>$tax->taxrate
-                        ]);
-                        $row->save();
-                    });
+                        ->lazyById()->each(function ($tax) {
+                            $row = Tax::firstOrCreate(['level' => $tax->level, 'country' => $tax->country, 'state' => $tax->state], [
+                                'level' => ($tax->level === null ? 0 : $tax->level),
+                                'name' => $tax->name,
+                                'country' => $tax->country,
+                                'state' => $tax->state,
+                                'taxrate' => $tax->taxrate
+                            ]);
+                            $row->save();
+                        });
                     Schema::drop('tax');
                 }
                 //pay_gateway table
                 if (Schema::hasTable('pay_gateway')) {
 
                     DB::table('pay_gateway')
-                      ->lazyById()->each(function ($pay_gateways) {
-                        $row = PaymentGateway::firstOrCreate(['name'=>$pay_gateways->name,'gateway'=>$pay_gateways->gateway,'accepted_currencies'=>$pay_gateways->accepted_currencies],[
-                            'name'=>$pay_gateways->name,
-                            'gateway'=>$pay_gateways->gateway,
-                            'accepted_currencies'=>$pay_gateways->accepted_currencies,
-                            'enabled'=>$pay_gateways->enabled,
-                            'allow_single'=>$pay_gateways->allow_single,
-                            'allow_recurrent'=>$pay_gateways->allow_recurrent,
-                            'test_mode'=>$pay_gateways->test_mode,
-                            'config'=>$pay_gateways->config
-                        ]);
-                        $row->save();
-                    });
+                        ->lazyById()->each(function ($pay_gateways) {
+                            $row = PaymentGateway::firstOrCreate(['name' => $pay_gateways->name, 'gateway' => $pay_gateways->gateway, 'accepted_currencies' => $pay_gateways->accepted_currencies], [
+                                'name' => $pay_gateways->name,
+                                'gateway' => $pay_gateways->gateway,
+                                'accepted_currencies' => $pay_gateways->accepted_currencies,
+                                'enabled' => $pay_gateways->enabled,
+                                'allow_single' => $pay_gateways->allow_single,
+                                'allow_recurrent' => $pay_gateways->allow_recurrent,
+                                'test_mode' => $pay_gateways->test_mode,
+                                'config' => $pay_gateways->config
+                            ]);
+                            $row->save();
+                        });
                     Schema::drop('pay_gateway');
                 }
                 //product_category table
                 if (Schema::hasTable('product_category')) {
 
                     DB::table('product_category')
-                      ->lazyById()->each(function ($product_category) {
-                        $row = ProductCategory::firstOrCreate(['title'=>$product_category->title,'description'=>$product_category->description],[
-                            'title'=>$product_category->title,
-                            'description'=>$product_category->description,
-                            'icon_url'=>$product_category->icon_url
-                        ]);
-                        $row->save();
-                    });
+                        ->lazyById()->each(function ($product_category) {
+                            $row = ProductCategory::firstOrCreate(['title' => $product_category->title, 'description' => $product_category->description], [
+                                'title' => $product_category->title,
+                                'description' => $product_category->description,
+                                'icon_url' => $product_category->icon_url
+                            ]);
+                            $row->save();
+                        });
                     Schema::drop('product_category');
                 }
                 //admin_group table
                 if (Schema::hasTable('admin_group')) {
 
                     DB::table('admin_group')
-                      ->lazyById()->each(function ($admin_group) {
-                        $row = AdminGroup::firstOrCreate(['name'=>$admin_group->name],[
-                            'name'=>$admin_group->name
-                        ]);
-                        $row->save();
-                    });
+                        ->lazyById()->each(function ($admin_group) {
+                            $row = AdminGroup::firstOrCreate(['name' => $admin_group->name], [
+                                'name' => $admin_group->name
+                            ]);
+                            $row->save();
+                        });
                     Schema::drop('admin_group');
                 }
                 //admin table
@@ -170,19 +169,19 @@ class SetUp extends Command
                 if (Schema::hasTable('admin')) {
 
                     DB::table('admin')
-                      ->lazyById()->each(function ($admin) {
-                        $user = User::create(
-                            [
-                                'id' => $admin->id,
-                                'name' => $admin->name,
-                                'email' => $admin->email,
-                                'role' => $admin->role,
-                                'password' => $admin->pass
-                            ]);
-                        $role = Role::firstOrCreate(['name' => $admin->role]);
-                        $this->info("Migraded user $user->name with the role of $admin->role");
-                        $user->assignRole($role);
-                    });
+                        ->lazyById()->each(function ($admin) {
+                            $user = User::create(
+                                [
+                                    'id' => $admin->id,
+                                    'name' => $admin->name,
+                                    'email' => $admin->email,
+                                    'role' => $admin->role,
+                                    'password' => $admin->pass
+                                ]);
+                            $role = Role::firstOrCreate(['name' => $admin->role]);
+                            $this->info("Migraded user $user->name with the role of $admin->role");
+                            $user->assignRole($role);
+                        });
                     Schema::drop('admin');
                 }
                 //client table
@@ -190,19 +189,19 @@ class SetUp extends Command
                 if (Schema::hasTable('client')) {
 
                     DB::table('client')
-                      ->lazyById()->each(function ($client) {
-                        $user = User::create(
-                            [
-                                'id' => $client->id,
-                                'name' => "$client->first_name $client->last_name",
-                                'email' => $client->email,
-                                'role' => $client->role,
-                                'password' => $client->pass
-                            ]);
-                        $role = Role::firstOrCreate(['name' => $client->role]);
-                        $this->info("Migraded user $user->name with the role of $client->role");
-                        $user->assignRole($role);
-                    });
+                        ->lazyById()->each(function ($client) {
+                            $user = User::create(
+                                [
+                                    'id' => $client->id,
+                                    'name' => "$client->first_name $client->last_name",
+                                    'email' => $client->email,
+                                    'role' => $client->role,
+                                    'password' => $client->pass
+                                ]);
+                            $role = Role::firstOrCreate(['name' => $client->role]);
+                            $this->info("Migraded user $user->name with the role of $client->role");
+                            $user->assignRole($role);
+                        });
                     Schema::drop('client');
                 }
             }
--- src/app/Http/Controllers/Client/HomeController.php
+++ src/app/Http/Controllers/Client/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class Home extends Controller
+class HomeController extends Controller
 {
     public function index()
     {
