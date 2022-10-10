629fe29e168f04466158fd44d90435fc3ef0f663
evrifaessa <hi@yagiz.dev>
2022-10-10 18:47:55 +0300

    Introduced our first two events: AfterClientLogin and ClientLoginFailed.

---
+++ app/Listeners/SendSuccessfulLoginEmail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\AfterClientLogin;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class SendSuccessfulLoginEmail
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\AfterClientLogin $event
+     * @return void
+     */
+    public function handle(AfterClientLogin $event)
+    {
+        // Pretend like we've sent an e-mail here.
+        Log::info("User " . $event->user['name'] . " (" . $event->user['email'] . ") has logged in.");
+    }
+}
