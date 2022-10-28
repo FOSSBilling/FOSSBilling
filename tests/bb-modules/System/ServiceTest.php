4ce004ee86fe8a25f4325c5269b2357c1d0b95be
Belle Aerni <belleaerni@gmail.com>
2022-10-27 17:35:39 -0700

    Should fix the tests

--- tests/bb-modules/System/ServiceTest.php
+++ tests/bb-modules/System/ServiceTest.php
@@ -176,6 +176,7 @@ class ServiceTest extends \BBTestCase {
         $di['array_get'] = $di->protect(function (array $array, $key, $default = null) use ($di) {
             return isset ($array[$key]) ? $array[$key] : $default;
         });
+        $di['mod_service'] = $di->protect(function() use($systemServiceMock) {return $systemServiceMock;});
 
         $systemServiceMock->setDi($di);
 
