2e592bf108f72a590d42d03f46fde5695b2bcede
Belle Aerni <belleaerni@gmail.com>
2022-12-28 03:39:21 -0800

    Update CWP.php

--- src/library/Server/Manager/CWP.php
+++ src/library/Server/Manager/CWP.php
@@ -323,10 +323,10 @@ class Server_Manager_CWP extends Server_Manager
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-		curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-		curl_setopt ($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+		curl_setopt($ch, CURLOPT_POST, 1);
 		$response = json_decode(curl_exec($ch), true);
 		curl_close($ch);
 
