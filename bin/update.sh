f420d7330bcdb46ef31f62b2447d604c53a517f6
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-01-16 10:34:50 +0100

    Remove docker and other old build files
    That shouldn't been used any more.
    - Testing have been replaced by Github Actions and I don't know if it is smart 5 year old docker images
    - We don't use travis anymore 
    - docker-compose has been replaced by a new one 
    - data/phpfpm.conf we should consider removing it. But at least udpdate to a php8 as PHP5 is EOL

--- bin/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-UPDATE_URL="https://www.fossbilling.org/version/latest_update.zip"
-
-wget -O update.zip -q $UPDATE_URL
-unzip -o update.zip
-php foss-update.php
-rm -rf update.zip
-rm -rf data/cache/*
