f402d096a0feb40fdf4667592305880e0c66b17d
Shamshid <shmshd12@gmail.com>
2023-01-18 12:05:22 +0400

    add gulp task for ckeditor

---
+++ src/modules/Wysiwyg/gulpfile.babel.js
@@ -0,0 +1,36 @@
+'use strict';
+
+import gulp from 'gulp';
+import clean from 'gulp-clean';
+import yargs from 'yargs';
+import upath from "upath";
+
+const {argv} = yargs
+  .options({
+    nodeModulesPath: {
+      'description': '<path> path to node_modules directory',
+      type: 'string',
+      requiresArgs: true,
+      required: false,
+    }
+  });
+
+const nodeModulesPath = upath.normalizeSafe(argv.nodeModulesPath);
+export const copyCKEditorBuild = function copyCKEditorBuild() {
+  return gulp.src(upath.joinSafe(nodeModulesPath, '@ckeditor/ckeditor5-build-classic/build/*'))
+    .pipe(gulp.dest('./assets/ckeditor'));
+}
+copyCKEditorBuild.description = 'copying built files to assets.';
+
+const cleanBuild = function cleanBuild() {
+  return gulp.src('./assets/ckeditor/*', {read: false})
+    .pipe(clean());
+}
+
+export const build = gulp.series(cleanBuild, copyCKEditorBuild);
+build.description = 'Build assets.';
+
+gulp.task('clean-build', cleanBuild);
+gulp.task('ckeditor', copyCKEditorBuild);
+
+export default build;
