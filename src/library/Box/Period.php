8f168f138b1228ca00ffc50a43a40b8173f80acd
Belle Aerni <belleaerni@gmail.com>
2023-01-26 15:02:57 -0800

    Translation work
    Added plural translation functionality, fixed the usage of placeholders, and cleaned up the functions

--- src/library/Box/Period.php
+++ src/library/Box/Period.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * FOSSBilling
  *
@@ -25,8 +26,8 @@ class Box_Period
     const PERIOD_QUARTER    = '3M';
     const PERIOD_BIANNUAL   = '6M';
     const PERIOD_ANNUAL     = '1Y';
-    const PERIOD_BIENNIAL	= '2Y';
-    const PERIOD_TRIENNIAL	= '3Y';
+    const PERIOD_BIENNIAL    = '2Y';
+    const PERIOD_TRIENNIAL    = '3Y';
 
     /**
      * Predefined periods
@@ -36,8 +37,8 @@ class Box_Period
         self::PERIOD_QUARTER    =>  3,
         self::PERIOD_BIANNUAL   =>  6,
         self::PERIOD_ANNUAL     =>  12,
-        self::PERIOD_BIENNIAL	=>	24,
-        self::PERIOD_TRIENNIAL	=>	36,
+        self::PERIOD_BIENNIAL    =>    24,
+        self::PERIOD_TRIENNIAL    =>    36,
     );
 
     private $unit;
@@ -45,7 +46,7 @@ class Box_Period
 
     public function __construct($code)
     {
-        if(strlen($code) != 2) {
+        if (strlen($code) != 2) {
             throw new \Box_Exception("Invalid period code. Period definition must be 2 chars length");
         }
 
@@ -54,11 +55,11 @@ class Box_Period
         $units = $this->getUnits();
         $qty = (int)$qty;
         $unit = strtoupper($unit);
-        if(!array_key_exists($unit, $units)) {
-            throw new \Box_Exception("Period Error. Unit :unit is not defined", array(':unit'=>$unit));
+        if (!array_key_exists($unit, $units)) {
+            throw new \Box_Exception("Period Error. Unit :unit is not defined", array(':unit' => $unit));
         }
 
-        if($qty < $units[$unit][0] || $qty > $units[$unit][1]) {
+        if ($qty < $units[$unit][0] || $qty > $units[$unit][1]) {
             $d = array(
                 ':qty'  =>  $qty,
                 ':unit'  =>  $unit,
@@ -85,18 +86,18 @@ class Box_Period
     public static function getPredefined($simple = true)
     {
         $periods = array(
-            self::PERIOD_WEEK       =>  array('rec_qty'=>1, 'title'=>__trans('Every week'), 'code'=>self::PERIOD_WEEK, 'rec_unit'=>self::UNIT_WEEK),
-            self::PERIOD_MONTH      =>  array('rec_qty'=>1, 'title'=>__trans('Every month'), 'code'=>self::PERIOD_MONTH, 'rec_unit'=>self::UNIT_MONTH),
-            self::PERIOD_QUARTER    =>  array('rec_qty'=>3, 'title'=>__trans('Every 3 months'), 'code'=>self::PERIOD_QUARTER, 'rec_unit'=>self::UNIT_MONTH),
-            self::PERIOD_BIANNUAL   =>  array('rec_qty'=>6, 'title'=>__trans('Every 6 months'), 'code'=>self::PERIOD_BIANNUAL, 'rec_unit'=>self::UNIT_MONTH),
-            self::PERIOD_ANNUAL     =>  array('rec_qty'=>1, 'title'=>__trans('Every year'), 'code'=>self::PERIOD_ANNUAL, 'rec_unit'=>self::UNIT_YEAR),
-            self::PERIOD_BIENNIAL	=>	array('rec_qty'=>2, 'title'=>__trans('Every 2 years'), 'code'=>self::PERIOD_BIENNIAL, 'rec_unit'=>self::UNIT_YEAR),
-            self::PERIOD_TRIENNIAL	=>	array('rec_qty'=>3, 'title'=>__trans('Every 3 years'), 'code'=>self::PERIOD_TRIENNIAL, 'rec_unit'=>self::UNIT_YEAR),
+            self::PERIOD_WEEK       =>  array('rec_qty' => 1, 'title' => __trans('Every week'), 'code' => self::PERIOD_WEEK, 'rec_unit' => self::UNIT_WEEK),
+            self::PERIOD_MONTH      =>  array('rec_qty' => 1, 'title' => __trans('Every month'), 'code' => self::PERIOD_MONTH, 'rec_unit' => self::UNIT_MONTH),
+            self::PERIOD_QUARTER    =>  array('rec_qty' => 3, 'title' => __trans('Every 3 months'), 'code' => self::PERIOD_QUARTER, 'rec_unit' => self::UNIT_MONTH),
+            self::PERIOD_BIANNUAL   =>  array('rec_qty' => 6, 'title' => __trans('Every 6 months'), 'code' => self::PERIOD_BIANNUAL, 'rec_unit' => self::UNIT_MONTH),
+            self::PERIOD_ANNUAL     =>  array('rec_qty' => 1, 'title' => __trans('Every year'), 'code' => self::PERIOD_ANNUAL, 'rec_unit' => self::UNIT_YEAR),
+            self::PERIOD_BIENNIAL    =>    array('rec_qty' => 2, 'title' => __trans('Every 2 years'), 'code' => self::PERIOD_BIENNIAL, 'rec_unit' => self::UNIT_YEAR),
+            self::PERIOD_TRIENNIAL    =>    array('rec_qty' => 3, 'title' => __trans('Every 3 years'), 'code' => self::PERIOD_TRIENNIAL, 'rec_unit' => self::UNIT_YEAR),
         );
 
-        if($simple) {
+        if ($simple) {
             $new = array();
-            foreach($periods as $pp) {
+            foreach ($periods as $pp) {
                 $new[$pp['code']] = $pp['title'];
             }
             $periods = $new;
@@ -116,23 +117,26 @@ class Box_Period
 
     public function getCode()
     {
-        return $this->qty.$this->unit;
+        return $this->qty . $this->unit;
     }
 
     public function getTitle()
     {
+        $qty = $this->qty;
+        $placeholders = [':number' => $qty];
+
         switch ($this->unit) {
             case self::UNIT_DAY:
-                $shift = __trans('Every :number day(s)', array(':number'=>$this->qty));
+                $shift = __pluralTrans('Every :number day', 'Every :number days', $qty, $placeholders);
                 break;
             case self::UNIT_WEEK:
-                $shift = __trans('Every :number week(s)', array(':number'=>$this->qty));
+                $shift = __pluralTrans('Every :number week', 'Every :number weeks', $qty, $placeholders);
                 break;
             case self::UNIT_MONTH:
-                $shift = __trans('Every :number month(s)', array(':number'=>$this->qty));
+                $shift = __pluralTrans('Every :number month', 'Every :number months', $qty, $placeholders);
                 break;
             case self::UNIT_YEAR:
-                $shift = __trans('Every :number year(s)', array(':number'=>$this->qty));
+                $shift = __pluralTrans('Every :number year', 'Every :number years', $qty, $placeholders);
                 break;
             default:
                 throw new \Box_Exception('Unit not defined');
@@ -140,6 +144,7 @@ class Box_Period
 
         return $shift;
     }
+
 
     public function getDays()
     {
@@ -176,7 +181,7 @@ class Box_Period
 
     public function getExpirationTime($now = NULL)
     {
-        if(NULL === $now) {
+        if (NULL === $now) {
             $now = time();
         }
 
