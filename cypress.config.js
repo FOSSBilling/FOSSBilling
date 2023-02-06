ef7830325cb5a6fb0182723ee42176246ea37dab
evrifaessa <hi@yagiz.dev>
2023-02-06 13:09:38 +0300

    Unused config value

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     experimentalStudio: true,
     credentials: {
       staff: {
