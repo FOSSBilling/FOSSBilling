f6b7ec3914af8653ae632a17ce8d9fa7bd378df6
Shamshid <shmshd12@gmail.com>
2023-02-16 09:32:04 +0400

    set purgecss for build and improve broswerlist coverage

--- src/themes/admin_default/postcss.config.js
+++ src/themes/admin_default/postcss.config.js
@@ -1,14 +1,25 @@
+const autoprefixer = require('autoprefixer');
+const purgeCss = require('@fullhuman/postcss-purgecss');
+const Encore = require('@symfony/webpack-encore');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {
-      browsers: [
-        '>1%',
+  plugins: [
+    Encore.isProduction() ? purgeCss({
+      content: [
+        '../../**/*.html.twig',
+        'assets/**/*.js',
+      ],
+      safelist: {
+        standard: [/flag(-.*)?/],
+      }
+    }) : false,
+    autoprefixer({
+      overrideBrowserslist: [
+        '>0.5%',
         'last 4 versions',
         'Firefox ESR',
-        'not ie < 9',
-      ],
-      flexbox: true
-    },
-  },
+        'not ie < 11',
+      ]
+    }),
+  ],
 }
