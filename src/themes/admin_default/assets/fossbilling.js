a4888e77e634a68f202ffc257e73122d9ba0b65e
Shamshid <shmshd12@gmail.com>
2023-02-16 09:33:31 +0400

    set main entrypoints for Encore

---
+++ src/themes/admin_default/assets/fossbilling.js
@@ -0,0 +1,14 @@
+import './scss/fossbilling.scss';
+
+import './js/sprite';
+import './js/jquery.min';
+import './js/ui/jquery.alerts';
+import 'spectrum-colorpicker/spectrum';
+import './js/forms/forms';
+import './js/jquery.scrollTo-min';
+import './js/jquery-ui';
+import '@tabler/core/src/js/tabler';
+import ApexCharts from 'apexcharts';
+import './js/fossbilling';
+
+globalThis.ApexCharts = ApexCharts;
