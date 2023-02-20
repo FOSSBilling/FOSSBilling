ac55922bc74f03acfae497b85dc7c544fdc7357a
Shamshid <shmshd12@gmail.com>
2023-02-20 12:29:36 +0400

    remove purgecss

--- src/themes/admin_default/postcss.config.js
+++ src/themes/admin_default/postcss.config.js
@@ -1,18 +1,7 @@
 const autoprefixer = require('autoprefixer');
-const purgeCss = require('@fullhuman/postcss-purgecss');
-const Encore = require('@symfony/webpack-encore');
 
 module.exports = {
   plugins: [
-    Encore.isProduction() ? purgeCss({
-      content: [
-        '../../**/*.html.twig',
-        'assets/**/*.js',
-      ],
-      safelist: {
-        standard: [/flag(-.*)?/],
-      }
-    }) : false,
     autoprefixer({
       overrideBrowserslist: [
         '>0.5%',
