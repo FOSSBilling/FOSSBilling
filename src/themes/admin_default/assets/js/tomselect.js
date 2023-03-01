a46636515ff00899f67d04b8e2d9519c1f9deda2
Shamshid <shmshd12@gmail.com>
2023-03-01 17:46:44 +0400

    replace axios with fetch and improve search

--- src/themes/admin_default/assets/js/tomselect.js
+++ src/themes/admin_default/assets/js/tomselect.js
@@ -1,5 +1,4 @@
 import TomSelect from 'tom-select';
-import axios from 'axios';
 
 globalThis.TomSelect = TomSelect;
 
@@ -14,26 +13,29 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     return `<div>${escape(data.text)}</div>`;
   }
-  let localeSelector = new TomSelect('.js-language-selector', {
-    copyClassesToDropdown: false,
-    controlClass: 'ts-control locale',
-    dropdownClass: 'dropdown-menu ts-dropdown',
-    optionClass: 'dropdown-item',
-    controlInput: false,
-    items: [bb.cookieRead('BBLANG')],
-    render: {
-      item: (data, escape) => {
-        return localeSelectorTemplate(data, escape);
+  let localeSelectorEl = document.querySelector('.js-language-selector');
+  if (localeSelectorEl !== null) {
+    let localeSelector = new TomSelect(".js-language-selector", {
+      copyClassesToDropdown: false,
+      controlClass: "ts-control locale",
+      dropdownClass: "dropdown-menu ts-dropdown",
+      optionClass: "dropdown-item",
+      controlInput: false,
+      items: [bb.cookieRead("BBLANG")],
+      render: {
+        item: (data, escape) => {
+          return localeSelectorTemplate(data, escape);
+        },
+        option: (data, escape) => {
+          return localeSelectorTemplate(data, escape);
+        },
       },
-      option: (data, escape) => {
-        return localeSelectorTemplate(data, escape);
-      },
-    },
-  });
-  localeSelector.on('change', (value) => {
-    bb.cookieCreate('BBLANG', value, 7);
-    bb.reload();
-  })
+    });
+    localeSelector.on("change", (value) => {
+      bb.cookieCreate("BBLANG", value, 7);
+      bb.reload();
+    });
+  }
 
 
   /**
@@ -42,41 +44,46 @@ document.addEventListener('DOMContentLoaded', () => {
   const autocompleteTemplate = (item, escape) => {
     return `<div class="py-2 d-flex">
                 <span>${escape(item.label)}</span>
+                <small class="text-muted ms-1">#${escape(item.value)}</span>
              </div>`;
   }
   let autocompleteSelectorEl = document.querySelector('.autocomplete-selector');
   if (autocompleteSelectorEl !== null) {
-    new TomSelect('.autocomplete-selector', {
+    new TomSelect(".autocomplete-selector", {
       copyClassesToDropdown: false,
-      dropdownClass: 'dropdown-menu ts-dropdown',
-      optionClass: 'dropdown-item',
-      valueField: 'value',
-      labelField: 'label',
-      searchField: 'label',
+      dropdownClass: "dropdown-menu ts-dropdown",
+      optionClass: "dropdown-item",
+      valueField: "value",
+      labelField: "label",
+      searchField: ["label", "value"],
       load: (query, callback) => {
         let items;
-        axios({
-          url: bb.restUrl(autocompleteSelectorEl.dataset.resturl),
-          params: {
-            per_page: 5,
-            search: query,
-            CSRFToken: autocompleteSelectorEl.dataset.csrf
-          },
-        }).then(function (response) {
-          items = Object.entries(response.data.result).map(([key, value]) => {
-            return {label: value, value: key}
+        let restUrl = new URL(
+          bb.restUrl(autocompleteSelectorEl.dataset.resturl)
+        );
+        restUrl.searchParams.append("search", query);
+        restUrl.searchParams.append(
+          "CSRFToken",
+          autocompleteSelectorEl.dataset.csrf
+        );
+        restUrl.searchParams.append("per_page", 5);
+        fetch(restUrl)
+          .then((response) => response.json())
+          .then((json) => {
+            items = Object.entries(json.result).map(([key, value]) => {
+              return { label: value, value: key };
+            });
+            callback(items);
           });
-          callback(items);
-        });
       },
       render: {
         option: function (item, escape) {
-          return autocompleteTemplate(item, escape)
+          return autocompleteTemplate(item, escape);
         },
         item: function (item, escape) {
           return `<span>${escape(item.label)}</span>`;
-        }
-      }
+        },
+      },
     });
   }
 
