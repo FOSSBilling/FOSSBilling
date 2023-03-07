f8a413ab573a1a5b315bb566bc7f5b224a8a65f0
Shamshid <shmshd12@gmail.com>
2023-03-07 13:34:58 +0400

    add comment for clarity on the added package

--- src/themes/admin_default/assets/fossbilling.js
+++ src/themes/admin_default/assets/fossbilling.js
@@ -2,7 +2,7 @@ import './scss/fossbilling.scss';
 
 import './js/sprite';
 import $ from 'jquery';
-import 'jquery.browser';
+import 'jquery.browser'; // Temporary package until the removal of jquery.alerts
 import './js/ui/jquery.alerts';
 import "@melloware/coloris/dist/coloris.css";
 import { coloris, init } from '@melloware/coloris';
