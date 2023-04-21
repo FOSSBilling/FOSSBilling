FOSSBilling/FOSSBilling

commit ca67aeb44f1ae5ee32e1b1f5598b5b561b3e6dcc

Refactor request handling to use native functionality (#1122)
Refactor request handling to remove custom library/Box/Request class where possible and instead use native PHP functionality.

src/index.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * FOSSBilling.
  *
@@ -17,8 +16,8 @@
 require_once __DIR__ . '/load.php';
 $di = include __DIR__ . '/di.php';
 
-$url = $di['request']->getQuery('_url') ?? '';
-$http_err_code = $di['request']->getQuery('_errcode') ?? null;
+$url = $_GET['_url'] ?? '';
+$http_err_code = $_GET['_errcode'] ?? '';
 
 $admin_prefix = $di['config']['admin_area_prefix'];
 if (0 === strncasecmp($url, $admin_prefix, strlen($admin_prefix))) {

src/di.php
@@ -367,7 +367,7 @@ $di['twig'] = $di->factory(function () use ($di) {
 $di['is_client_logged'] = function () use ($di) {
     if (!$di['auth']->isClientLoggedIn()) {
         $api_str = '/api/';
-        $url = $di['request']->getQuery('_url');
+        $url = $_GET['_url'] ?? null;
 
         if (0 === strncasecmp($url, $api_str, strlen($api_str))) {
             // Throw Exception if api request
@@ -395,7 +395,7 @@ $di['is_client_logged'] = function () use ($di) {
 $di['is_admin_logged'] = function () use ($di) {
     if (!$di['auth']->isAdminLoggedIn()) {
         $api_str = '/api/';
-        $url = $di['request']->getQuery('_url', null, '');
+        $url = $_GET['_url'] ?? '';
         if (0 === strncasecmp($url, $api_str, strlen($api_str))) {
             // Throw Exception if api request
             throw new Exception('Admin is not logged in');
@@ -739,7 +739,7 @@ $di['translate'] = $di->protect(function ($textDomain = '') use ($di) {
 
 /*
  * Creates a CSV export of data from a specified table and sends it to the browser.
- * 
+ *
  * @param string $table Name of the table to export data from
  * @param string $outputName Name of the exported CSV file
  * @param array $headers Optional array of column headers for the CSV file

src/library/Box/App.php
@@ -265,7 +265,8 @@ class Box_App
      */
     protected function checkAllowedURLs()
     {
-        $REQUEST_URI = $this->di['request']->getServer('REQUEST_URI');
+        $REQUEST_URI = $_SERVER['REQUEST_URI'] ?? null;
+
         $allowedURLs = $this->di['config']['maintenance_mode']['allowed_urls'];
         $rootUrl = $this->di['config']['url'];
 
@@ -323,7 +324,7 @@ class Box_App
      */
     protected function checkAdminPrefix()
     {
-        $REQUEST_URI = $this->di['request']->getServer('REQUEST_URI');
+        $REQUEST_URI = $_SERVER['REQUEST_URI'] ?? null;
         $adminPrefix = $this->di['config']['admin_area_prefix'];
         $rootUrl = $this->di['config']['url'];
 

src/library/Box/Pagination.php
@@ -41,9 +41,9 @@ class Box_Pagination implements InjectionAwareInterface
     public function getSimpleResultSet($q, $values, $per_page = 100, $page = null)
     {
         if (is_null($page)){
-            $page = $this->di['request']->getQuery('page', "int", 1);
+            $page = $_GET['page'] ?? 1;
         }
-        $per_page = $this->di['request']->getQuery('per_page', "int", $per_page);
+        $per_page = $_GET['per_page'] ?? $per_page;
 
         $offset = ($page - 1) * $per_page;
 
@@ -67,8 +67,8 @@ class Box_Pagination implements InjectionAwareInterface
 
     public function getAdvancedResultSet($q, $values, $per_page = 100)
     {
-        $page = $this->di['request']->getQuery('page', "int", 1);
-        $per_page = $this->di['request']->getQuery('per_page', "int", $per_page);
+        $page = $page = $_GET['page'] ?? 1;
+        $per_page = $_GET['per_page'] ?? $per_page;
 
         $offset = ($page - 1) * $per_page;
         $q = str_replace('SELECT ', 'SELECT SQL_CALC_FOUND_ROWS ', $q);

src/library/Box/Request.php
@@ -9,29 +9,12 @@
  * Copyright BoxBilling, Inc 2011-2021
  *
  * This source file is subject to the Apache-2.0 License that is bundled
- * with this source code in the file LICENSE
+ * with this source code in the file LICENSE.
  */
-
 
 class Box_Request implements \Box\InjectionAwareInterface
 {
     protected $di;
-    protected $_data = array();
-    protected $_header = array();
-    protected $_server = array();
-    protected $_request = array();
-    protected $_post = array();
-    protected $_get = array();
-    protected $_cookie = array();
-    protected $_method = 'post';
-
-    public function __construct()
-    {
-        $this->_get = $_GET;
-        $this->_post = $_POST;
-        $this->_request = $_REQUEST;
-        $this->_server = $_SERVER;
-    }
 
     public function setDi($di)
     {
@@ -41,212 +24,6 @@ class Box_Request implements \Box\InjectionAwareInterface
     public function getDi()
     {
         return $this->di;
-    }
-
-    public function get($name = null, $filters = null, $defaultValue = null)
-    {
-        if (empty($name)) {
-            return $this->_request;
-        }
-        $value = isset($this->_request[$name]) ? $this->_request[$name] : $defaultValue;
-        return $this->_filterValue($value, $filters);
-    }
-
-    public function getPost($name = null, $filters = null, $defaultValue = null)
-    {
-        if (empty($name)) {
-            return $this->_post;
-        }
-        $value = isset($this->_post[$name]) ? $this->_post[$name] : $defaultValue;
-        return $this->_filterValue($value, $filters);
-    }
-
-    public function getQuery($name = null, $filters = null, $defaultValue = null)
-    {
-        if (empty($name)) {
-            return $this->_get;
-        }
-
-        $value = isset($this->_get[$name]) ? $this->_get[$name] : $defaultValue;
-        return $this->_filterValue($value, $filters);
-    }
-
-    /**
-     * Returns the available headers in the request
-     * @return array
-     */
-    public function getHeaders()
-    {
-        $list = getallheaders();
-        return is_array($list) ? $list : array();
-    }
-
-    public function getPut($name = null, $filters = null, $defaultValue = null)
-    {
-        parse_str($this->getRawBody(), $put);
-        if(empty($name)) {
-            return $put;
-        }
-
-        $value = isset($put[$name]) ? $put[$name] : $defaultValue;
-        return $this->_filterValue($value, $filters);
-    }
-
-    /**
-     * @param string $name
-     */
-    public function getServer($name)
-    {
-        return isset($this->_server[$name]) ? $this->_server[$name] : null;
-    }
-
-    public function has($name)
-    {
-        return isset($this->_request[$name]);
-    }
-
-    public function hasPost($name)
-    {
-        return isset($this->_post[$name]);
-    }
-
-    public function hasPut($name)
-    {
-        parse_str($this->getRawBody(), $put);
-        if(is_array($put) && isset($put[$name])) {
-            return true;
-        }
-        return false;
-    }
-
-    public function hasQuery($name)
-    {
-        return isset($this->_get[$name]);
-    }
-
-    /**
-     * @param string $name
-     */
-    public function hasServer($name)
-    {
-        return isset($this->_server[$name]);
-    }
-
-    /**
-     * Gets HTTP header from request data
-     * @param string $header
-     * @return string
-     */
-    public function getHeader($header)
-    {
-        $headers = $this->getHeaders();
-        return isset($headers[$header]) ? $headers[$header] : null;
-    }
-
-    /**
-     * Gets HTTP schema (http/https)
-     * @return string
-     */
-    public function getScheme()
-    {
-        return $this->getServer('HTTPS') ? 'https' : 'http';
-    }
-    
-    /**
-     * Checks whether request has been made using ajax. Checks if $_SERVER[‘HTTP_X_REQUESTED_WITH’]==’XMLHttpRequest’
-     * @return bool
-     */
-    public function isAjax()
-    {
-        return ($this->getServer('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest');
-    }
-
-    /**
-     * Checks whether request has been made using SOAP
-     * @return bool
-     */
-    public function isSoapRequested()
-    {
-        if($this->hasServer('HTTP_SOAPACTION')) {
-            return true;
-        }
-
-        if($this->getServer('CONTENT_TYPE') == 'application/soap+xml') {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Checks whether request has been made using any secure layer
-     * @return bool
-     */
-    public function isSecureRequest()
-    {
-        return ($this->getScheme() == 'https');
-    }
-
-    /**
-     * Gets HTTP raw request body
-     * @return string
-     */
-    public function getRawBody()
-    {
-        return file_get_contents('php://input');
-    }
-
-    /**
-     * Gets decoded JSON HTTP raw request body
-     * @return string
-     */
-    public function getJsonRawBody()
-    {
-        $result = json_decode($this->getRawBody(), 1);
-        if($result) {
-            return $result;
-        }
-
-        $_messages = array(
-            JSON_ERROR_NONE => 'No error has occurred',
-            JSON_ERROR_DEPTH => 'The maximum stack depth has been exceeded',
-            JSON_ERROR_STATE_MISMATCH => 'Invalid or malformed JSON',
-            JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
-            JSON_ERROR_SYNTAX => 'Syntax error',
-            JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
-        );
-        $error = json_last_error();
-        $msg = isset($_messages[$error]) ? $_messages[$error] : 'Error decoding json';
-        throw new RuntimeException($msg);
-    }
-
-    /**
-     * Gets active server address IP
-     * @return string
-     */
-    public function getServerAddress()
-    {
-        $address = $this->getServer('SERVER_ADDR');
-        return ($address) ? $address : '127.0.0.1';
-    }
-
-    /**
-     * Gets active server name
-     * @return string
-     */
-    public function getServerName()
-    {
-        $address = $this->getServer('SERVER_NAME');
-        return ($address) ? $address : 'localhost';
-    }
-
-    /**
-     * Gets information about schema, host and port used by the request
-     * @return string
-     */
-    public function getHttpHost()
-    {
-        return $this->getServer('HTTP_HOST');
     }
 
     /**
@@ -259,10 +36,10 @@ class Box_Request implements \Box\InjectionAwareInterface
     {
         $address = null;
         if($trustForwardedHeader) {
-            $address = $this->getServer('HTTP_X_FORWARDED_FOR');
+            $address = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? null;
         }
         if(is_null($address)) {
-            $address = $this->getServer('REMOTE_ADDR');
+            $address = $_SERVER['REMOTE_ADDR'] ?? null;
         }
         if(is_string($address)) {
             if(strpos($address, ',') !== false) {
@@ -270,115 +47,6 @@ class Box_Request implements \Box\InjectionAwareInterface
             }
         }
         return $address;
-    }
-
-    /**
-     * Gets HTTP method which request has been made
-     * @return string
-     */
-    public function getMethod()
-    {
-        return $this->getServer('REQUEST_METHOD');
-    }
-
-    /**
-     * Gets HTTP URI which request has been made
-     * @return string
-     */
-    public function getURI()
-    {
-        return $this->getServer('REQUEST_URI');
-    }
-
-    /**
-     * Gets HTTP user agent used to made the request
-     * @return string
-     */
-    public function getUserAgent()
-    {
-        return $this->getServer('HTTP_USER_AGENT');
-    }
-
-    /**
-     * Check if HTTP method match any of the passed methods
-     * @param string | array
-     * @return bool
-     */
-    public function isMethod($methods)
-    {
-        $current = $this->getMethod();
-        if(is_array($methods)) {
-            foreach($methods as $method) {
-                if($current == $method) {
-                    return true;
-                }
-            }
-            return false;
-        }
-        return ($current == $methods);
-    }
-
-    /**
-     * Checks whether HTTP method is POST. if $_SERVER[‘REQUEST_METHOD’]==’POST’
-     * @return bool
-     */
-    public function isPost()
-    {
-        return ($this->getMethod() == 'POST');
-    }
-
-    /**
-     * Checks whether HTTP method is GET. if $_SERVER[‘REQUEST_METHOD’]==’GET’
-     * @return bool
-     */
-    public function isGet()
-    {
-        return ($this->getMethod() == 'GET');
-    }
-
-    /**
-     * Checks whether HTTP method is PUT. if $_SERVER[‘REQUEST_METHOD’]==’PUT’
-     * @return bool
-     */
-    public function isPut()
-    {
-        return ($this->getMethod() == 'PUT');
-    }
-
-    /**
-     * Checks whether HTTP method is PATCH. if $_SERVER[‘REQUEST_METHOD’]==’PATCH’
-     * @return bool
-     */
-    public function isPatch()
-    {
-        return ($this->getMethod() == 'PATCH');
-    }
-
-    /**
-     * Checks whether HTTP method is HEAD. if $_SERVER[‘REQUEST_METHOD’]==’HEAD’
-     * @return bool
-     */
-    public function isHead()
-    {
-        return ($this->getMethod() == 'HEAD');
-    }
-
-    /**
-     * Checks whether HTTP method is DELETE. if $_SERVER[‘REQUEST_METHOD’]==’DELETE’
-     * @return bool
-     */
-    public function isDelete()
-    {
-        return ($this->getMethod() == 'DELETE');
-    }
-
-    /**
-     * Checks whether HTTP method is OPTIONS. if $_SERVER[‘REQUEST_METHOD’]==’OPTIONS’
-     * @return bool
-     */
-    public function isOptions()
-    {
-        return ($this->getMethod() == 'OPTIONS');
     }
 
     /**
@@ -416,186 +84,5 @@ class Box_Request implements \Box\InjectionAwareInterface
             }
         }
         return $files;
-    }
-
-    /**
-     * Gets the web page that refers the active request. ie: https://www.google.com
-     * @return string
-     */
-    public function getHTTPReferer()
-    {
-        return $this->getServer('HTTP_REFERER');
-    }
-
-    /**
-     * Gets array with mime/types and their quality accepted by the browser/client from $_SERVER[‘HTTP_ACCEPT’]
-     * @return array
-     */
-    public function getAcceptableContent()
-    {
-        return $this->getServer('HTTP_ACCEPT');
-    }
-
-    /**
-     * Gets best mime/type accepted by the browser/client from $_SERVER[‘HTTP_ACCEPT’]
-     * @return array
-     */
-    public function getBestAccept()
-    {
-        //@todo
-    }
-
-    /**
-     * Gets charsets array and their quality accepted by the browser/client from $_SERVER[‘HTTP_ACCEPT_CHARSET’]
-     * @return array
-     */
-    public function getClientCharsets()
-    {
-        //@todo
-    }
-
-    /**
-     * Gets best charset accepted by the browser/client from $_SERVER[‘HTTP_ACCEPT_CHARSET’]
-     * @return string
-     */
-    public function getBestCharset()
-    {
-        //@todo
-    }
-
-    /**
-     * Gets languages array and their quality accepted by the browser/client from $_SERVER[‘HTTP_ACCEPT_LANGUAGE’]
-     * @return array
-     */
-    public function getLanguages()
-    {
-        $language_header = $this->getServer('HTTP_ACCEPT_LANGUAGE');
-        return $this->_getQualityHeader($language_header, 'language');
-    }
-
-    /**
-     * Gets best language accepted by the browser/client from $_SERVER[‘HTTP_ACCEPT_LANGUAGE’]
-     * @return string
-     */
-    public function getBestLanguage()
-    {
-        //@todo
-    }
-
-    /**
-     * Gets auth info accepted by the browser/client from $_SERVER[‘PHP_AUTH_USER’]
-     * @return array
-     */
-    public function getBasicAuth()
-    {
-        $username = null;
-        $password = null;
-
-        if ($this->hasServer('PHP_AUTH_USER')) {
-            $username = $_SERVER['PHP_AUTH_USER'];
-            $password = $_SERVER['PHP_AUTH_PW'];
-        } elseif ($this->hasServer('HTTP_AUTHENTICATION')) {
-            $auth = $this->getServer('HTTP_AUTHENTICATION');
-            if (strpos(strtolower($auth), 'basic') === 0) {
-                list($username,$password) = explode(':',base64_decode(substr($auth, 6)));
-            }
-        }
-
-        return array(
-            'username'  =>  $username,
-            'password'  =>  $password,
-        );
-    }
-
-    /**
-     * Gets auth info accepted by the browser/client from $_SERVER[‘PHP_AUTH_DIGEST’]
-     * @return array
-     */
-    public function getDigestAuth()
-    {
-        // mod_php
-        if ($this->hasServer('PHP_AUTH_DIGEST')) {
-            $digest = $this->getServer('PHP_AUTH_DIGEST');
-            // most other servers
-        } elseif ($this->hasServer('HTTP_AUTHENTICATION')) {
-            $auth = $this->getServer('HTTP_AUTHENTICATION');
-            if (strpos(strtolower($auth),'digest')===0) {
-                $digest = substr($auth, 7);
-            }
-        }
-
-        // protect against missing data
-        $needed_parts = array('nonce'=>1, 'nc'=>1, 'cnonce'=>1, 'qop'=>1, 'username'=>1, 'uri'=>1, 'response'=>1);
-        $data = array();
-        $keys = implode('|', array_keys($needed_parts));
-
-        preg_match_all('@(' . $keys . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $digest, $matches, PREG_SET_ORDER);
-
-        foreach ((array) $matches as $m) {
-            $data[$m[1]] = $m[3] ? $m[3] : $m[4];
-            unset($needed_parts[$m[1]]);
-        }
-
-        return $needed_parts ? false : $data;
-    }
-
-    /**
-     * Process a request header and return an array of values with their qualities
-     * @param string $q
-     * @return array
-     */
-    protected function _getQualityHeader($header, $q)
-    {
-        $return = array();
-        $parts = preg_split('/,\\s*/', $header);
-
-        foreach($parts as $part) {
-            $headerParts = explode(';', $part);
-            if(isset($headerParts[1]) === true) {
-                $quality = substr($headerParts[1], 2);
-            } else {
-                $quality = 1;
-            }
-
-            $return[] = array($q => $headerParts[0], 'quality' => $quality);
-        }
-
-        return $return;
-    }
-
-    /**
-     * Process a request header and return the one with best quality
-     * @return string
-     */
-    protected function _getBestQuality($header)
-    {
-        //@todo
-    }
-
-    /**
-     * @todo implement filtering
-     * @param $value - value to filter
-     * @param string|array $filters - list of filter to apply on value
-     */
-    private function _filterValue($value, $filters)
-    {
-        return $value;
-    }
-}
-
-
-if (!function_exists('getallheaders'))
-{
-    function getallheaders()
-    {
-        $headers = '';
-        foreach ($_SERVER as $name => $value)
-        {
-            if (substr($name, 0, 5) == 'HTTP_')
-            {
-                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
-            }
-        }
-        return $headers;
     }
 }

src/library/Box/Requirements.php
@@ -65,7 +65,7 @@ class Box_Requirements implements \Box\InjectionAwareInterface
     public function getInfo()
     {
         $data = array();
-        $data['ip']             = isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : null;
+        $data['ip']             = $_SERVER['SERVER_ADDR'] ?? null;
         $data['PHP_OS']         = PHP_OS;
         $data['PHP_VERSION']    = PHP_VERSION;
 

src/load.php
@@ -345,7 +345,7 @@ ini_set('html_errors', false);
 ini_set('error_log', PATH_LOG . '/php_error.log');
 
 $isApache = (function_exists('apache_get_version')) ? true : false;
-$serverSoftware = (isset($_SERVER['SERVER_SOFTWARE'])) ? $_SERVER['SERVER_SOFTWARE'] : '';
+$serverSoftware = ($_SERVER['SERVER_SOFTWARE']) ?? '';
 
 if ($isApache or (false !== stripos($serverSoftware, 'apache')) or (false !== stripos($serverSoftware, 'litespeed'))) {
     if (!file_exists(PATH_ROOT . '/.htaccess')) {

src/modules/Api/Service.php
@@ -48,7 +48,7 @@ class Service implements \Box\InjectionAwareInterface
         ';
         $values = [
             'ip' => $request->getClientAddress(),
-            'request' => $request->getURI(),
+            'request' => $_SERVER['REQUEST_URI'] ?? null,
         ];
 
         return $this->di['db']->exec($sql, $values);

commit a1e2d23cf730a73380a6c8e422e04fb83ee081b8

Fix HTTP response error code bug
Update index to fix bug in error code handling introduced in #1122 by restoring default $_GET value for errcode to null.

src/index.php
@@ -17,7 +17,7 @@ require_once __DIR__ . '/load.php';
 $di = include __DIR__ . '/di.php';
 
 $url = $_GET['_url'] ?? '';
-$http_err_code = $_GET['_errcode'] ?? '';
+$http_err_code = $_GET['_errcode'] ?? null;
 
 $admin_prefix = $di['config']['admin_area_prefix'];
 if (0 === strncasecmp($url, $admin_prefix, strlen($admin_prefix))) {
@@ -40,6 +40,7 @@ if (!is_null($http_err_code)) {
             echo $app->show404($e);
             break;
         default:
+            $http_err_code = intval($http_err_code);
             http_response_code($http_err_code);
             $e = new Box_Exception('HTTP Error :err_code occured while attempting to load :url', [':err_code' => $http_err_code, ':url' => $url], $http_err_code);
             echo $app->render('error', ['exception' => $e]);