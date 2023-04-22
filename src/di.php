FOSSBilling/FOSSBilling

commit f291ad0dc7613c865faf7b52192054a6d8ed9ef7

Slightly improved twig locale handling  (#1060)
* Better handle the lack of the intl extension

* Use cookies to set twig locale

This allows for more of the system to be translated to the user's locale

src/di.php
@@ -309,7 +309,7 @@ $di['twig'] = $di->factory(function () use ($di) {
 
     // Get internationalisation settings from config, or use sensible defaults for
     // missing required settings.
-    $locale = $config['i18n']['locale'] ?? 'en_US';
+    $locale = $_COOKIE['BBLANG'] ?? $config['i18n']['locale'] ?? 'en_US';
     $timezone = $config['i18n']['timezone'] ?? 'UTC';
     $date_format = !empty($config['i18n']['date_format']) ? strtoupper($config['i18n']['date_format']) : 'MEDIUM';
     $time_format = !empty($config['i18n']['time_format']) ? strtoupper($config['i18n']['time_format']) : 'SHORT';
@@ -336,7 +336,16 @@ $di['twig'] = $di->factory(function () use ($di) {
     $twig->addExtension($box_extensions);
     $twig->getExtension(CoreExtension::class)->setTimezone($timezone);
 
-    $dateFormatter = new \IntlDateFormatter($locale, constant("\IntlDateFormatter::$date_format"), constant("\IntlDateFormatter::$time_format"), $timezone, null, $datetime_pattern);
+    try {
+        $dateFormatter = new \IntlDateFormatter($locale, constant("\IntlDateFormatter::$date_format"), constant("\IntlDateFormatter::$time_format"), $timezone, null, $datetime_pattern);
+    } catch (\Symfony\Polyfill\Intl\Icu\Exception\MethodArgumentValueNotImplementedException $e) {
+        if (($config['i18n']['locale'] ?? 'en_US') == 'en_US') {
+            $dateFormatter = new \IntlDateFormatter('en', constant("\IntlDateFormatter::$date_format"), constant("\IntlDateFormatter::$time_format"), $timezone, null, $datetime_pattern);
+        } else {
+            throw new \Box_Exception("It appears you are trying to use FOSSBilling without the php intl extension enabled. FOSSBilling includes a polyfill for the intl extension, however it does not support :locale. Please enable the intl extension.", [':locale' => $config['i18n']['locale']]);
+        }
+    }
+
     $twig->addExtension(new IntlExtension($dateFormatter));
 
     // add globals
@@ -354,7 +363,6 @@ $di['twig'] = $di->factory(function () use ($di) {
     $twig->addGlobal('CSRFToken', $token);
     $twig->addGlobal('request', $_GET);
     $twig->addGlobal('guest', $di['api_guest']);
-
     return $twig;
 });
 

commit c270c61ab7ea95a620ee1caa68cbc7d2d57e4d16

Fix locale date/time formatting issue
Update locale handling to fix minor issue introduced by #1060 when specifying default/fallback locale.

src/di.php
@@ -327,7 +327,7 @@ $di['twig'] = $di->factory(function () use ($di) {
         $dateFormatter = new \IntlDateFormatter($locale, constant("\IntlDateFormatter::$date_format"), constant("\IntlDateFormatter::$time_format"), $timezone, null, $datetime_pattern);
     } catch (\Symfony\Polyfill\Intl\Icu\Exception\MethodArgumentValueNotImplementedException) {
         if (($config['i18n']['locale'] ?? 'en_US') == 'en_US') {
-            $dateFormatter = new \IntlDateFormatter('en', constant("\IntlDateFormatter::$date_format"), constant("\IntlDateFormatter::$time_format"), $timezone, null, $datetime_pattern);
+            $dateFormatter = new \IntlDateFormatter('en_US', constant("\IntlDateFormatter::$date_format"), constant("\IntlDateFormatter::$time_format"), $timezone, null, $datetime_pattern);
         } else {
             throw new \Box_Exception("It appears you are trying to use FOSSBilling without the php intl extension enabled. FOSSBilling includes a polyfill for the intl extension, however it does not support :locale. Please enable the intl extension.", [':locale' => $config['i18n']['locale']]);
         }