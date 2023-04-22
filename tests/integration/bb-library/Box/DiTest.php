6dd91cf37358656be392e63342ca7254e0ac8aba
evrifaessa <hi@yagiz.dev>
2023-04-22 12:16:22 +0300

    Caught a typo

--- tests/integration/bb-library/Box/DiTest.php
+++ tests/integration/bb-library/Box/DiTest.php
@@ -32,7 +32,7 @@ class DiTest extends PHPUnit\Framework\TestCase
         $this->assertTrue(isset($di['mod']));
         $this->assertTrue(isset($di['mod_config']));
         $this->assertInstanceOf('Box\\Mod\\Cron\\Service', $di['mod_service']('cron'));
-        $this->assertInstanceOf('Symfony\Component\HttpClient\HttpClient', $di['http_client'];
+        $this->assertInstanceOf('Symfony\Component\HttpClient\HttpClient', $di['http_client']);
         $this->assertInstanceOf('\Box_Extension', $di['extension']);
         $this->assertInstanceOf('\Box_Update', $di['updater']);
         $this->assertInstanceOf('\Server_Package', $di['server_package']);
