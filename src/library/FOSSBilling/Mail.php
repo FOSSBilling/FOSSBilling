b973010596d0badaa5148df41004dada7230c675
Yağızhan <yagizhan49@protonmail.com>
2023-04-27 23:31:52 +0300

    Types

--- src/library/FOSSBilling/Mail.php
+++ src/library/FOSSBilling/Mail.php
@@ -52,7 +52,7 @@ class FOSSBilling_Mail
      * @param string $bodyHTML        The HTML content of the email body.
      * @param string|null $transport  (optional) The name of the transport to use for sending the email.
      * @param string|null $dsn        (optional) The DSN to use for sending the email. (See: https://symfony.com/doc/current/mailer.html#using-a-3rd-party-transport)
-     * 
+     *
      * @return void
      */
     public function __construct(array|string $from, array|string $to, string $subject, string $bodyHTML, string|null $transport, string|null $dsn = null)
@@ -89,7 +89,7 @@ class FOSSBilling_Mail
      *
      * @return void
      */
-    public function addTo(string|array $toAddresses)
+    public function addTo(string|array $toAddresses): void
     {
         $this->email->addto($toAddresses);
     }
@@ -101,7 +101,7 @@ class FOSSBilling_Mail
      *
      * @return void
      */
-    public function addCc(string|array $ccAddresses)
+    public function addCc(string|array $ccAddresses): void
     {
         $this->email->addCc($ccAddresses);
     }
@@ -113,7 +113,7 @@ class FOSSBilling_Mail
      *
      * @return void
      */
-    public function addBcc(string|array $bccAddresses)
+    public function addBcc(string|array $bccAddresses): void
     {
         $this->email->addBcc($bccAddresses);
     }
@@ -125,22 +125,22 @@ class FOSSBilling_Mail
      *
      * @return void
      */
-    public function addReplyto(string|array $replyToAddresses)
+    public function addReplyto(string|array $replyToAddresses): void
     {
         $this->email->addReplyTo($replyToAddresses);
     }
 
     /**
      * Set the priority of the email message. Can be an integer between 1 and 5, with 1 being the highest priority.
-     * It's recomended to use Symfony's pre-defined constants instead of a specific integer, but either should work. (Example: Email::PRIORITY_HIGH)
+     * It's recommended to use Symfony's pre-defined constants instead of a specific integer, but either should work. (Example: Email::PRIORITY_HIGH)
      *
      * @param int $priority The priority of the email message, represented as an integer between 1 and 5.
      *
-     * @throws \Box_Exception if the provided priority is invalid. 
-     * 
+     * @throws \Box_Exception if the provided priority is invalid.
+     *
      * @return void
      */
-    public function setPriority(int $priority)
+    public function setPriority(int $priority): void
     {
         if (is_int($priority) && $priority >= Email::PRIORITY_HIGHEST && $priority <= Email::PRIORITY_LOWEST) {
             $this->email->priority($priority);
@@ -154,7 +154,7 @@ class FOSSBilling_Mail
      *
      * @param array|null $options An optional array of options specific to the chosen transport method.
      * @throws \Box_Exception If the transport method is unknown or if required options for the selected transport aren't defined
-     * 
+     *
      * @return void
      */
     public function send(array|null $options = null)
@@ -201,10 +201,10 @@ class FOSSBilling_Mail
      * @param array $options An associative array of SMTP options including 'smtp_host', 'smtp_port', 'smtp_username', and 'smtp_password'.
      *
      * @throws \Box_Exception if the SMTP host or port is not configured.
-     * 
+     *
      * @return string A string representing the DSN for the SMTP transport.
      */
-    private function __smtpDsn(array $options)
+    private function __smtpDsn(array $options): string
     {
         if (empty($options['smtp_host']) || empty($options['smtp_port'])) {
             throw new \Box_Exception('SMTP host or port is not configured');
