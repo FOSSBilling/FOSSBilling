99cd09284cbab17856d989b1e720cfccc67d5afa
evrifaessa <hi@yagiz.dev>
2023-05-05 20:14:06 +0300

    Changed the header

--- src/library/Box/DbLoggedPDOStatement.php
+++ src/library/Box/DbLoggedPDOStatement.php
@@ -1,16 +1,11 @@
 <?php
 /**
- * FOSSBilling.
+ * Copyright 2022-2023 FOSSBilling
+ * Copyright 2011-2021 BoxBilling, Inc. 
+ * SPDX-License-Identifier: Apache-2.0
  *
  * @copyright FOSSBilling (https://www.fossbilling.org)
- * @license   Apache-2.0
- *
- * Copyright FOSSBilling 2022
- * This software may contain code previously used in the BoxBilling project.
- * Copyright BoxBilling, Inc 2011-2021
- *
- * This source file is subject to the Apache-2.0 License that is bundled
- * with this source code in the file LICENSE.
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
  */
 
 class Box_DbLoggedPDOStatement extends PDOStatement
