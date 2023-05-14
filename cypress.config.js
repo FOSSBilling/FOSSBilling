0aed9d2d9b3ce1828bcc0ecbb732a648b046a961
evrifaessa <hi@yagiz.dev>
2023-05-14 12:05:20 +0300

    Moved Cypress tests to /tests/cypress

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   projectId: "zap1kb",
   e2e: {
     baseUrl: 'http://127.0.0.1:8000',
+    specPattern: 'tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     experimentalStudio: true,
     experimentalRunAllSpecs: true,
     credentials: {
