ab051ac4dfbbe20e73d04df45d1d6206bf98adaf
Belle Aerni <belleaerni@gmail.com>
2023-07-30 08:50:11 -0700

    Allow null for that type
    Honestly doing so makes ZERO sense, but it matches the current behavior.

--- src/library/Server/Package.php
+++ src/library/Server/Package.php
@@ -10,7 +10,7 @@
 
 class Server_Package
 {
-    private string $name                   = 'FOSSBilling';
+    private ?string $name           = 'FOSSBilling';
     private $quota                  = NULL;
     private $bandwidth              = NULL;
     
