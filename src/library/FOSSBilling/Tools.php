176cfbf0101e84c0c27a0010486d3e8dbf58c3dc
Albin Varghese <albin8285@gmail.com>
2023-09-19 19:53:20 +0530

    fix: correct http detection

--- src/library/FOSSBilling/Tools.php
+++ src/library/FOSSBilling/Tools.php
@@ -291,6 +291,6 @@ class Tools
         $protocol = $_SERVER['HTTPS'] ?? $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? $_SERVER['REQUEST_SCHEME'] ?? '';
 
         // $_SERVER['HTTPS'] will be set to `on` to indicate HTTPS and the other to will be set to `https`, so either one means we are connected via HTTPS.
-        return (strcasecmp($protocol, 'on') === 0 || strcasecmp($protocol, 'https'));
+        return (strcasecmp($protocol, 'on') === 0 || strcasecmp($protocol, 'https') === 0);
     }
 }
