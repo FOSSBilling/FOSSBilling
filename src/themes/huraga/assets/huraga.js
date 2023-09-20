36873a85fda9da4c3387c42cccbf22b29fa2179a
Shamshid <shmshd12@gmail.com>
2023-09-20 14:51:45 +0400

    use tom-select for language selector

--- src/themes/huraga/assets/huraga.js
+++ src/themes/huraga/assets/huraga.js
@@ -1,7 +1,8 @@
 import './scss/huraga.scss';
 
-import * as bootstrap from "bootstrap"
 import $ from 'jquery';
+import * as bootstrap from 'bootstrap';
+import '../../admin_default/assets/js/tomselect'
 
 globalThis.$ = globalThis.jQuery = $;
 globalThis.bootstrap = bootstrap;
