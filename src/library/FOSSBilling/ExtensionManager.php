ab1a59ce14ca234e346865ace5ff125c74a35f83
Belle Aerni <belleaerni@gmail.com>
2023-09-24 23:39:20 -0700

    Remove unneeded use statement

--- src/library/FOSSBilling/ExtensionManager.php
+++ src/library/FOSSBilling/ExtensionManager.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace FOSSBilling;
 
-use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Contracts\Cache\ItemInterface;
 
 class ExtensionManager implements InjectionAwareInterface
