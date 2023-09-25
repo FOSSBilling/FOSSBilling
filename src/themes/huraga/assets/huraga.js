dc7a0b80529dd2324a3d7a6521a259f949496fad
Shamshid <shmshd12@gmail.com>
2023-09-25 23:25:15 +0400

    improve public pages by replacing jquery and bb-jquery

--- src/themes/huraga/assets/huraga.js
+++ src/themes/huraga/assets/huraga.js
@@ -2,7 +2,8 @@ import './scss/huraga.scss';
 
 import $ from 'jquery';
 import * as bootstrap from 'bootstrap';
-import '../../admin_default/assets/js/tomselect'
+import '../../admin_default/assets/js/tomselect';
+import '../../admin_default/assets/js/fossbilling';
 
 globalThis.$ = globalThis.jQuery = $;
 globalThis.bootstrap = bootstrap;
