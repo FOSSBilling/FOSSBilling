d8ed19eac2d5b9ae3ca07d76bd91be3b2914be62
Shamshid <shmshd12@gmail.com>
2023-10-08 00:11:40 +0400

    add flashMessage method

--- src/themes/huraga/assets/huraga.js
+++ src/themes/huraga/assets/huraga.js
@@ -8,10 +8,33 @@ import '../../admin_default/assets/js/fossbilling';
 globalThis.$ = globalThis.jQuery = $;
 globalThis.bootstrap = bootstrap;
 
-/**
- * Enable Bootstrap Tooltip
- */
 document.addEventListener('DOMContentLoaded', () => {
+  /**
+   * Enable Bootstrap Tooltip
+   */
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
   [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+  /**
+   * Manage flash message to show after page reload
+   */
+  globalThis.flashMessage = ({message = '', reload = false, type = 'info'}) => {
+    let key = 'flash-message';
+    let sessionMessage = sessionStorage.getItem(key);
+    if (message === '' && sessionMessage) {
+      FOSSBilling.message(sessionMessage, type);
+      sessionStorage.removeItem(key);
+      return;
+    }
+    if (message) {
+      sessionStorage.setItem(key, message);
+      if (typeof reload === 'boolean' && reload) {
+        bb.redirect();
+      } else if (typeof reload === 'string') {
+        bb.redirect(reload);
+      }
+    }
+  }
+  flashMessage({});
 });
+
