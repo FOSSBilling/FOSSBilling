b26d100bdeade71902ed4be76c1809cfd2205e47
Christoph Schläpfer <c.schlaepfer@datact.ch>
2023-10-13 12:35:52 +0200

    Fixed "DOM text reinterpreted as HTML error"

--- src/themes/admin_default/assets/js/fossbilling.js
+++ src/themes/admin_default/assets/js/fossbilling.js
@@ -326,35 +326,53 @@ globalThis.FOSSBilling = {
         color = 'primary';
     }
 
-    const container = document.querySelector('.toast-container'); // Get the existing container or create if not present
+    const container = document.querySelector('.toast-container');
 
     const element = document.createElement('div');
-    container.appendChild(element); // Append the toast container to the container
-    element.classList.add('toast', 'show'); // Add 'show' class to display the toast immediately
+    container.appendChild(element);
+    element.classList.add('toast', 'show');
     element.setAttribute('role', 'alert');
     element.setAttribute('aria-live', 'assertive');
     element.setAttribute('aria-atomic', 'true');
 
-    element.innerHTML = `
-      <div class="toast-header">
-        <span class="p-2 border border-light bg-${color} rounded-circle me-2"></span>
-        <strong class="me-auto">System message</strong>
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-      </div>
-      <div class="toast-body">${message}</div>
-    `;
+    // Create header div and its children elements
+    const headerDiv = document.createElement('div');
+    headerDiv.className = 'toast-header';
+
+    const spanEl = document.createElement('span');
+    spanEl.className = `p-2 border border-light bg-${color} rounded-circle me-2`;
+    headerDiv.appendChild(spanEl);
+
+    const strongEl = document.createElement('strong');
+    strongEl.className = 'me-auto';
+    strongEl.textContent = 'System message';
+    headerDiv.appendChild(strongEl);
+
+    const closeButton = document.createElement('button');
+    closeButton.type = 'button';
+    closeButton.className = 'btn-close';
+    closeButton.setAttribute('data-bs-dismiss', 'toast');
+    closeButton.setAttribute('aria-label', 'Close');
+    headerDiv.appendChild(closeButton);
+
+    element.appendChild(headerDiv);
+
+    // Create body div and set its text content
+    const bodyDiv = document.createElement('div');
+    bodyDiv.className = 'toast-body';
+    bodyDiv.textContent = message;  // Safely set the message content
+    element.appendChild(bodyDiv);
 
     element.addEventListener('hidden.bs.toast', () => {
-      container.removeChild(element); // Remove the toast element from the container when it's hidden
+      container.removeChild(element);
     });
-
-    container.appendChild(element); // Append the toast to the container
 
     // Create a new Bootstrap toast instance and show it
     const toast = new bootstrap.Toast(element);
     toast.show();
   }
 };
+
 
 
 
