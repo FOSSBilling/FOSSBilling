cf86159d0234c9fc5fecdb52c6e1ba9afb701c9a
Belle Aerni <belleaerni@gmail.com>
2023-10-17 13:09:42 -0700

    I guess that's not needed now?
    Arighty then

--- src/load.php
+++ src/load.php
@@ -229,7 +229,6 @@ function registerSentry(array $config): void
      * Here we validate that the DSN is correctly set and that error reporting is enabled before passing it off to the Sentry SDK.
      * It may look a bit odd, but the DSN placeholder value here is split into two strings and concatenated so we can easily perform a `sed` replacement of the placeholder without it effecting this check
      */
-    /* @phpstan-ignore-next-line (The empty check is added to catch a possible edge-case where CI may the placeholder with an empty string) */
     if ($config['debug_and_monitoring']['report_errors'] && $sentryDSN !== '--replace--this--' . 'during--release--process--' && !empty($sentryDSN)) {
         // Per Sentry documentation, not setting this results in the SDK simply not sending any information.
         $options['dsn'] = $sentryDSN;
