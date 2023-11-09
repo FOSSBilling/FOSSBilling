c379837d8a05e3637b24a7bd9aa05f263c77d60e
Belle Aerni <belleaerni@gmail.com>
2023-11-09 01:50:31 -0800

    Update rector config to include tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,10 @@
 <?php
+putenv("APP_ENV=test");
 define('PATH_TESTS', dirname(__FILE__));
-require_once dirname(__FILE__) . '/../src/load.php';
-$config = include dirname(__FILE__) . '/../src/config.php';
 
+require_once dirname(__FILE__) . '/../src/load.php';
 require_once dirname(__FILE__) . '/../src/vendor/autoload.php';
+$config = include dirname(__FILE__) . '/../src/config.php';
 
 define('BB_DB_NAME', $config['db']['name']);
 define('BB_DB_USER', $config['db']['user']);
