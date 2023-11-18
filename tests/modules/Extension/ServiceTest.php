b8d755db754773744ceee1df983c1daa1a0e5223
Belle Aerni <belleaerni@gmail.com>
2023-11-18 14:34:12 -0800

    Use ArrayAdapter

--- tests/modules/Extension/ServiceTest.php
+++ tests/modules/Extension/ServiceTest.php
@@ -643,7 +643,7 @@ class ServiceTest extends \BBTestCase {
         $di['crypt'] = $cryptMock;
         $di['tools'] = $toolsMock;
         $di['config'] = array('salt' => '');
-        $di['cache'] = new \Symfony\Component\Cache\Adapter\FilesystemAdapter('sf_cache', 24 * 60 * 60, PATH_CACHE);
+        $di['cache'] = new \Symfony\Component\Cache\Adapter\ArrayAdapter();
 
         $this->service->setDi($di);
 
@@ -673,7 +673,7 @@ class ServiceTest extends \BBTestCase {
 
         $di = new \Pimple\Container();
         $di['db'] = $dbMock;
-        $di['cache'] = new \Symfony\Component\Cache\Adapter\FilesystemAdapter('sf_cache', 24 * 60 * 60, PATH_CACHE);
+        $di['cache'] = new \Symfony\Component\Cache\Adapter\ArrayAdapter();
 
         $this->service->setDi($di);
         $result = $this->service->getConfig($data['ext']);
@@ -718,7 +718,7 @@ class ServiceTest extends \BBTestCase {
         $di['events_manager'] = $eventMock;
         $di['logger'] = new \Box_Log();
         $di['config'] = array('salt' => '');
-        $di['cache'] = new \Symfony\Component\Cache\Adapter\FilesystemAdapter('sf_cache', 24 * 60 * 60, PATH_CACHE);
+        $di['cache'] = new \Symfony\Component\Cache\Adapter\ArrayAdapter();
 
         $serviceMock->setDi($di);
         $result = $serviceMock->setConfig($data);
