58447709b073d3cb167575006e86cbccf2cbcd42
Belle Aerni <belleaerni@gmail.com>
2024-02-05 19:47:46 -0800

    Fix

--- tests/Modules/Cron/AdminTest.php
+++ tests/Modules/Cron/AdminTest.php
@@ -15,10 +15,10 @@ final class AdminTest extends TestCase
         $result = Request::makeRequest('admin/cron/info');
         $this->assertTrue($result->wasSuccessful(), $result->generatePHPUnitMessage());
         if (!empty($result->getResult()['last_cron_exec'])) {
-            $firstDate = new DateTime($result->getResult()['last_cron_exec']);
+            $firstDate = new \DateTime($result->getResult()['last_cron_exec']);
         } else {
             // Cron didn't have a last execution set, so let's just make up that it was an hour ago
-            $firstDate = new DateTime();
+            $firstDate = new \DateTime();
             $firstDate->modify('-1 hour');
         }
 
@@ -34,7 +34,7 @@ final class AdminTest extends TestCase
         $this->assertTrue($result->wasSuccessful(), $result->generatePHPUnitMessage());
         $this->assertNotEmpty($result->getResult()['last_cron_exec']);
 
-        $newDate = new DateTime($result->getResult()['last_cron_exec']);
+        $newDate = new \DateTime($result->getResult()['last_cron_exec']);
         $this->assertGreaterThan($firstDate, $newDate, "Cron's last execution time was not incremented");
     }
 }
