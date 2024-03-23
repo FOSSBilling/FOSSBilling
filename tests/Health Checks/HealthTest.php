a04414a058bd4ebbfde313f3e0b9c80a8de4d8a6
Belle Aerni <belleaerni@gmail.com>
2024-03-22 19:43:12 -0700

    Fixed a couple of new errors

--- tests/Health Checks/HealthTest.php
+++ tests/Health Checks/HealthTest.php
@@ -1,3 +1,4 @@
+
 <?php
 
 declare(strict_types=1);
@@ -5,8 +6,36 @@ declare(strict_types=1);
 use APIHelper\Request;
 use PHPUnit\Framework\TestCase;
 
+
 final class HealthTest extends TestCase
 {
+    private array $test_urls = [
+        '' => 200,
+        'order' => 200,
+        'news' => 200,
+        'tos' => 200,
+        'privacy-policy' => 200,
+        'sitemap.xml' => 200,
+        'contact-us' => 200,
+        'login' => 200,
+        'signup' => 200,
+        'password-reset' => 200
+    ];
+
+    public function testUrls(): void
+    {
+        $baseUrl = getenv('APP_URL');
+        foreach ($this->test_urls as $url => $code) {
+            $ch = curl_init($baseUrl . $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_exec($ch);
+            $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+
+            $this->assertEquals($code, $responseCode, "Failed asserting that the URL '{$url}' returned a {$code} response code.");
+        }
+    }
+
     public function testIsFOSSBillingWorking(): void
     {
         $result = Request::makeRequest('guest/system/company');
@@ -18,6 +47,6 @@ final class HealthTest extends TestCase
     {
         $result = Request::makeRequest('admin/system/is_behind_on_patches');
         $this->assertTrue($result->wasSuccessful(), $result->generatePHPUnitMessage());
-        $this->assertNotTrue($result->getResult()); // This should return false to indicate there are no patches available, meaning the `last_patch` number is correct for fresh installs.
+        $this->assertFalse($result->getResult()); // This should return false to indicate there are no patches available, meaning the `last_patch` number is correct for fresh installs.
     }
 }
