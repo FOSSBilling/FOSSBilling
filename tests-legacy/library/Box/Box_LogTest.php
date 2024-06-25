cc353bf72121e0aea335346588c0d5e963be852b
Anuril <chris+github@cleverly.ch>
2024-06-25 13:14:36 +0200

    Fixed test for recursive masking

--- tests-legacy/library/Box/Box_LogTest.php
+++ tests-legacy/library/Box/Box_LogTest.php
@@ -33,6 +33,7 @@ class Box_LogTest extends PHPUnit\Framework\TestCase
                                             ],
                                         ],
                                     ],
+                                    'key' => 'SecondVerySecretValue',
                                 ],
                             ],
                         ],
@@ -42,59 +43,40 @@ class Box_LogTest extends PHPUnit\Framework\TestCase
         ];
         $this->valid_params = $valid_params;
         $this->regression_test_params = $regression_test_params;
-        $this->regression_limit = 10;
+        $this->regression_limit = 8;
 
     }
     // test maskParams method
     public function testMaskParams(): void
     {
-        // test normal masking
-        $params = $this->valid_params;
+        // test normal masking with $this->valid_params and $log_message
+        $log_message = 'This is a log message';
+        $params = [];
+        $params = array_merge($this->valid_params, ['message' => $log_message]);
 
-        $this->assertEquals(
-            [
-                'foo' => [
-                    'bar' => [
-                        'baz' => '********',
-                    ],
-                ],
-            ],
-            $this->log->maskParams($params)
-        );
+        // target output
+        $output = [
+            'foo' => 'bar',
+            'baz' => 'qux',
+            'key' => '********',
+            'message' => 'This is a log message',
+        ];
+
+        $this->assertEquals($output, $this->log->maskParams($params));
     }
 
     // test maskParams method with regression limit
     public function testMaskParamsWithRegressionLimit(): void
     {
         // test masking with regression limit
+        $params = [];
         $params = $this->regression_test_params;
 
-        $this->assertEquals(
-            [
-                'foo' => [
-                    'bar' => [
-                        'baz' => [
-                            'lorem' => [
-                                'ipsum' => [
-                                    'dolor' => [
-                                        'sit' => [
-                                            'amet' => [
-                                                'foo' => [
-                                                    'bar' => [
-                                                        'key' => '********',
-                                                    ],
-                                                ],
-                                            ],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-            $this->log->maskParams($params, 0, $this->regression_limit)
-        );
+        // Expect FossBilling\Exception to be thrown
+        $this->expectException(FOSSBilling\Exception::class);
+        $this->log->maskParams($params, 0, $this->regression_limit);
+        
+
     }
 }
 
