332a3969c1e050bd3ad8ba17be2b080e24baf076
ChristianGabs <developer.webservice@gmail.com>
2024-06-30 16:24:31 +0100

    Remove wrapper for pimple console and setup console rework the console.php run php fix

--- src/console.php
+++ src/console.php
@@ -12,56 +12,68 @@ if (php_sapi_name() !== 'cli') {
     exit('This script can only be run from the command line.');
 }
 
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputOption;
+
 require_once __DIR__ . '/load.php';
 $di = include __DIR__ . '/di.php';
 
 $di['translate']();
 
-$di->register(new CristianG\PimpleConsole\ServiceProvider(), [
-    /*
-     * Set the console application name. Defaults to 'Console'
-     * @param  string
-     */
-    'console.name' => 'FOSSBilling Console Commands',
-    /*
-     * Set the console application version. Defaults to '2.0'
-     * @param  string
-     */
-    'console.version' => $di['mod_service']('system')->getVersion(),
-    /*
-     * Set console application list
-     * @param  array
-     */
-    'console.classes' => function ($di): array {
-        try {
-            $modules = $di['mod_service']('extension')->getCoreAndActiveModules();
-        } catch (Exception) {
-            $modules = $di['mod']('extension')->getCoreModules();
+$di['console'] = function () use ($di) {
+    $container = [
+        'console.name' => 'FossBilling Console Commands',
+        'console.version' => $di['mod_service']('system')->getVersion(),
+        'console.allow_namespace' => true,
+    ];
+    $console = new Application($container['console.name'], $container['console.version']);
+
+    if ($container['console.allow_namespace']) {
+        $console->getDefinition()->addOption(
+            new InputOption(
+                'namespace',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Specify namespace for the console'
+            )
+        );
+        $console->setDefaultCommand('list');
+        $namespace = array_reduce($_SERVER['argv'], function ($carry, $arg) {
+            return str_starts_with($arg, '--namespace=') ? substr($arg, strlen('--namespace=')) : $carry;
+        }, '');
+
+        if (!empty($namespace) && class_exists($namespace)) {
+            $class = new $namespace();
+            $class->setDi($di);
+            $console->add($class);
+
+            return $console;
         }
-        $fullyQualifiedClassName = [];
-        // Dynamically load the commands from the modules
-        foreach ($modules as $module) {
-            $cap = ucfirst($module);
-            $commands = glob(__DIR__ . '/modules/' . $cap . '/Console/*.php');
-            foreach ($commands as $command) {
-                $commandClass = basename($command, '.php');
-                // Construct the fully qualified class name
-                $fullyQualifiedClassName[] = "\\Box\\Mod\\$cap\\Console\\$commandClass";
+    }
+
+    try {
+        $modules = $di['mod_service']('extension')->getCoreAndActiveModules();
+    } catch (Exception $e) {
+        $modules = $di['mod']('extension')->getCoreModules();
+    }
+
+    // Dynamically load the commands from the modules
+    foreach ($modules as $module) {
+        $cap = ucfirst($module);
+        $commands = glob(__DIR__ . '/modules/' . $cap . '/Console/*.php');
+        foreach ($commands as $command) {
+            $commandClass = basename($command, '.php');
+            $class = "\\Box\\Mod\\$cap\\Console\\$commandClass";
+            if (class_exists($class)) {
+                $instance = new $class();
+                $instance->setDi($di);
+                $console->add($instance);
             }
         }
+    }
 
-        return $fullyQualifiedClassName;
-    },
-    /*
-     * Set namespace command --namespace="\Namespace\Run" to be provided on command
-     * @param  bool
-     */
-    'console.allow_namespace' => FOSSBilling\Config::getProperty('console.allow_namespace', true),
-    /*
-     * Set your DI new Pimple\Container() for your app to be loaded before execute
-     */
-    'console.di' => $di,
-]);
+    return $console;
+};
 
 $console = $di['console'];
 
