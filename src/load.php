9819730c8222efecf6253470b155dde64e719cfa
Adam Daley <admdly@users.noreply.github.com>
2025-03-05 20:28:52 +0000

    Refactor initial application loading
    Refactor `load.php` to simplify loading, clarify loading, and to use available Symfony components. Additionally, update `index.php` as appropriate.
    
    Add Symfony HttpFoundation as supporting dependency and for future functionality.

--- src/load.php
+++ src/load.php
@@ -1,5 +1,6 @@
 <?php
 
+declare(strict_types=1);
 /**
  * Copyright 2022-2025 FOSSBilling
  * Copyright 2011-2021 BoxBilling, Inc.
@@ -14,63 +15,40 @@ use FOSSBilling\Environment;
 use FOSSBilling\SentryHelper;
 use FOSSBilling\Tools;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
+use Symfony\Component\HttpFoundation\Request;
 use Whoops\Handler\PrettyPageHandler;
 use Whoops\Run;
-
-const PATH_ROOT = __DIR__;
-const PATH_VENDOR = PATH_ROOT . DIRECTORY_SEPARATOR . 'vendor';
-const PATH_LIBRARY = PATH_ROOT . DIRECTORY_SEPARATOR . 'library';
-const PATH_THEMES = PATH_ROOT . DIRECTORY_SEPARATOR . 'themes';
-const PATH_MODS = PATH_ROOT . DIRECTORY_SEPARATOR . 'modules';
-const PATH_LANGS = PATH_ROOT . DIRECTORY_SEPARATOR . 'locale';
-const PATH_UPLOADS = PATH_ROOT . DIRECTORY_SEPARATOR . 'uploads';
-const PATH_CONFIG = PATH_ROOT . DIRECTORY_SEPARATOR . 'config.php';
-
-/*
- * Check configuration exists, and is valid.
- */
-function checkConfig(): void
-{
-    $filesystem = new Filesystem();
-    // Check if configuration is available, and redirect to installer if not.
-    if (!$filesystem->exists(PATH_CONFIG)) {
-        if ($filesystem->exists('install/index.php')) {
-            // Build the base URL for the installation, including the protocol and hostname.
-            $base_url = 'http' . ((isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] === 'on' || $_SERVER['HTTPS'] == 1)) || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') ? 's' : '') . '://' . (!empty($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : $_SERVER['HTTP_HOST']);
-
-            // Append the directory name to the base URL.
-            $base_url .= rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
-
-            // Use the base URL to redirect to the installer, sending HTTP 307 to indicate a temporary redirect.
-            header('Location: ' . $base_url . '/install/index.php', true, 307);
-        } else {
-            throw new Exception('The FOSSBilling configuration file is empty or invalid.', 3);
-        }
-    }
-}
 
 /*
  * Check if the installer is present.
  */
 function checkInstaller(): void
 {
+    global $filesystem, $request;
+
+    // Check if /install directory exists before installation has been completed.
+    if (!$filesystem->exists(PATH_CONFIG) && $filesystem->exists(Path::normalize('install/install.php'))) {
+        // Redirect to the installer.
+        header('Location: ' . $request->getSchemeAndHttpHost() . $request->getBasePath() . '/install/install.php', true, 307);
+        exit;
+    }
+
+    // Ignore this check if not in production environment.
     if (!Environment::isProduction()) {
         return;
     }
 
-    $filesystem = new Filesystem();
-
     // Check if /install directory still exists after installation has been completed.
-    if ($filesystem->exists(PATH_CONFIG) && $filesystem->exists('install/install.php')) {
+    if ($filesystem->exists(PATH_CONFIG) && $filesystem->exists(Path::normalize('install/install.php'))) {
         // Throw exception only if debug mode is NOT enabled.
-        $config = require PATH_CONFIG;
-        if (!$config['debug_and_monitoring']['debug']) {
+        if (!empty(Config::getProperty('debug_and_monitoring.debug'))) {
             throw new Exception('For security reasons, you have to delete the install directory before you can use FOSSBilling.', 2);
         }
     }
 
     // If the config file exists and not install.php, but the install folder does, perform some cleanup.
-    if ($filesystem->exists(PATH_CONFIG) && $filesystem->exists('install') && !DEBUG) {
+    if ($filesystem->exists(PATH_CONFIG) && $filesystem->exists(Path::normalize('install')) && !DEBUG) {
         $filesystem->remove('install');
     }
 }
@@ -80,15 +58,14 @@ function checkInstaller(): void
  */
 function checkLegacyFiles(): void
 {
-    $filesystem = new Filesystem();
+    global $filesystem;
 
     // Detect old files and folders from legacy BoxBilling or FOSSBilling installations.
     $toCheck = ['bb-data', 'bb-library', 'bb-locale', 'bb-modules', 'bb-themes', 'bb-uploads', 'bb-cron.php', 'bb-di.php', 'bb-load.php', 'bb-config.php'];
     $legacyFound = null;
     foreach ($toCheck as $path) {
-        if ($filesystem->exists($path)) {
+        if ($filesystem->exists(Path::normalize($path))) {
             $legacyFound = true;
-
             break;
         }
     }
@@ -100,30 +77,46 @@ function checkLegacyFiles(): void
 }
 
 /*
- * Check hard requirements such as PHP version, Composer packages, etc.
- */
-function checkRequirements(): void
-{
-    // Check for Composer packages / vendor folder.
-    if (!file_exists(PATH_VENDOR)) {
-        throw new Exception('The composer packages are missing.', 1);
-    }
-}
-
-/*
  * Check if SSL required, and enforce if so.
  */
 function checkSSL(): void
 {
-    $config = include PATH_CONFIG;
-    if (isset($config['security']['force_https']) && $config['security']['force_https'] && !Environment::isCLI()) {
-        if (!Tools::isHTTPS()) {
-            $hostname = $_SERVER['HTTP_X_FORWARDED_HOST'] ?? $_SERVER['HTTP_HOST'] ?? '';
-            if (!empty($hostname) && !empty($_SERVER['REQUEST_URI'])) {
-                $url = 'https://' . $hostname . $_SERVER['REQUEST_URI'];
-                header('Location: ' . $url);
-                exit;
-            }
+    global $request;
+
+    if (!empty(Config::getProperty('security.force_https')) && Config::getProperty('security.force_https') && !Environment::isCLI()) {
+        if (!$request->isSecure()) {
+            header('Location: ' . 'https://' . $request->getHost() . $request->getRequestUri());
+            exit;
+        }
+    }
+}
+
+/*
+ * Check if the update patcher needs to be run.
+ */
+function checkUpdatePatcher(): void
+{
+    global $di, $request;
+
+    $version = FOSSBilling\Version::VERSION;
+    if ($di['cache']->getItem('updatePatcher')->isHit() && $version === $di['cache']->getItem('updatePatcher')->get())
+    {
+        exit('The update patcher has already been run for this version.');
+    }
+
+    if ($request->getPathInfo() == '/run-patcher' || $request->query->get('_url') === '/run-patcher') {
+        $patcher = new FOSSBilling\UpdatePatcher();
+        $patcher->setDi($di);
+
+        try {
+            $patcher->applyConfigPatches();
+            $patcher->applyCorePatches();
+            $di['tools']->emptyFolder(PATH_CACHE);
+            $di['cache']->getItem('updatePatcher')->set(FOSSBilling\Version::VERSION);
+
+            exit('Any missing config migrations or database patches have been applied and the cache has been cleared.');
+        } catch (Exception $e) {
+            exit('An error occurred while attempting to apply patches: <br>' . $e->getMessage());
         }
     }
 }
@@ -133,11 +126,11 @@ function checkSSL(): void
  */
 function checkWebServer(): void
 {
-    $filesystem = new Filesystem();
+    global $filesystem;
 
     // Check for missing required .htaccess on Apache and Apache-compatible web servers.
-    $webSever = SentryHelper::estimateWebServer();
-    if ($webSever === 'Apache' || $webSever === 'Litespeed') {
+    $webServer = SentryHelper::estimateWebServer();
+    if ($webServer === 'Apache' || $webServer === 'Litespeed') {
         if (!$filesystem->exists('.htaccess')) {
             throw new Exception('Missing .htaccess file', 5);
         }
@@ -149,11 +142,6 @@ function checkWebServer(): void
  */
 function errorHandler(int $number, string $message, string $file, int $line)
 {
-    // Just some housekeeping to ensure a few things we rely on are loaded.
-    require_once PATH_LIBRARY . DIRECTORY_SEPARATOR . 'FOSSBilling' . DIRECTORY_SEPARATOR . 'ErrorPage.php';
-    require_once PATH_LIBRARY . DIRECTORY_SEPARATOR . 'FOSSBilling' . DIRECTORY_SEPARATOR . 'SentryHelper.php';
-    require_once PATH_LIBRARY . DIRECTORY_SEPARATOR . 'FOSSBilling' . DIRECTORY_SEPARATOR . 'Environment.php';
-
     // If it's an exception, handle it. Otherwise we don't need to do anything as PHP will log it for us.
     if ($number === E_RECOVERABLE_ERROR) {
         exceptionHandler(new ErrorException($message, $number, 0, $file, $line));
@@ -167,10 +155,7 @@ function errorHandler(int $number, string $message, string $file, int $line)
  */
 function exceptionHandler(Exception|Error $e)
 {
-    // Just some housekeeping to ensure a few things we rely on are loaded.
-    require_once PATH_LIBRARY . DIRECTORY_SEPARATOR . 'FOSSBilling' . DIRECTORY_SEPARATOR . 'ErrorPage.php';
-    require_once PATH_LIBRARY . DIRECTORY_SEPARATOR . 'FOSSBilling' . DIRECTORY_SEPARATOR . 'SentryHelper.php';
-    require_once PATH_LIBRARY . DIRECTORY_SEPARATOR . 'FOSSBilling' . DIRECTORY_SEPARATOR . 'Environment.php';
+    global $filesystem;
 
     if (Environment::isTesting()) {
         echo $e->getMessage() . PHP_EOL;
@@ -190,7 +175,7 @@ function exceptionHandler(Exception|Error $e)
         return false;
     }
 
-    if (defined('DEBUG') && DEBUG && file_exists(PATH_VENDOR)) {
+    if (defined('DEBUG') && DEBUG && $filesystem->exists(PATH_VENDOR)) {
         /**
          * If advanced debugging is enabled, print Whoops instead of our error page.
          * flip/whoops documentation: https://github.com/filp/whoops/blob/master/docs/API%20Documentation.md.
@@ -215,55 +200,112 @@ function exceptionHandler(Exception|Error $e)
 }
 
 /*
- *
- * Initialize App.
- *
+ * Pre-initialization.
  */
+function preInit() : void
+{
+    // Define root path.
+    define('PATH_ROOT', __DIR__);
 
-// Define custom error handlers.
-set_exception_handler(exceptionHandler(...));
-set_error_handler(errorHandler(...));
+    // Check vendor folder exists and load Composer autoloader.
+    define('PATH_VENDOR', PATH_ROOT . DIRECTORY_SEPARATOR . 'vendor');
+    if (!file_exists(PATH_VENDOR)) {
+        throw new Exception('The composer packages are missing.', 1);
+    }
+    require PATH_VENDOR . DIRECTORY_SEPARATOR . 'autoload.php';
 
-// Enabled during setup, is then overridden once we have loaded the config.
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
+    // Define global paths.
+    define('PATH_LIBRARY', Path::normalize(PATH_ROOT . '/library/'));
+    define('PATH_THEMES', Path::normalize(PATH_ROOT . '/themes/'));
+    define('PATH_MODS', Path::normalize(PATH_ROOT . '/modules/'));
+    define('PATH_LANGS', Path::normalize(PATH_ROOT . '/locale/'));
+    define('PATH_UPLOADS', Path::normalize(PATH_ROOT . '/uploads/'));
+    define('PATH_CONFIG', Path::normalize(PATH_ROOT . '/config.php'));
 
-// Check hard requirements.
-checkRequirements();
+    // Load required FOSSBilling libraries.
+    require Path::normalize(PATH_LIBRARY . '/FOSSBilling/ErrorPage.php');
+    require Path::normalize(PATH_LIBRARY . '/FOSSBilling/SentryHelper.php');
+    require Path::normalize(PATH_LIBRARY . '/FOSSBilling/Environment.php');
+    require Path::normalize(PATH_LIBRARY . '/FOSSBilling/Config.php');
+    require Path::normalize(PATH_LIBRARY . '/FOSSBilling/Tools.php');
+}
 
-// Requirements met - load required packages/files.
-require PATH_VENDOR . DIRECTORY_SEPARATOR . 'autoload.php';
+/*
+ * Initialize the application.
+ */
+function init() : void
+{
+    // Define custom error handlers.
+    set_exception_handler(exceptionHandler(...));
+    set_error_handler(errorHandler(...));
+
+    // Initialize required Symfony components.
+    global $filesystem, $request;
+    $filesystem = new Filesystem();
+    $request = Request::createFromGlobals();
+
+    // Check config exists and throw exception unless installer exists.
+    if (!$filesystem->exists(PATH_CONFIG) && !$filesystem->exists(Path::normalize('install/install.php'))) {
+        throw new Exception('The FOSSBilling configuration file is empty or invalid.', 3);
+    }
+
+    // Set globals and relevant settings based on the config.
+    date_default_timezone_set(Config::getProperty('i18n.timezone', 'UTC'));
+    define('ADMIN_PREFIX', Config::getProperty('admin_area_prefix'));
+    define('DEBUG', (bool) Config::getProperty('debug_and_monitoring.debug', false));
+    define('PATH_DATA', Path::normalize(Config::getProperty('path_data')));
+    define('PATH_CACHE',Path::normalize(PATH_DATA . '/cache'));
+    define('PATH_LOG', Path::normalize(PATH_DATA . '/log'));
+    define('INSTANCE_ID', Config::getProperty('info.instance_id', 'Unknown'));
+
+    // Set the system URL.
+    $scheme = Config::getProperty('security.force_https', true) || $request->isSecure() ? 'https://' : 'http://';
+    define('SYSTEM_URL', $scheme . Config::getProperty('url'));
+
+    // Set the default interface.
+    define('BIND_TO', Tools::getDefaultInterface());
+
+    // Initial setup and checks passed, now we setup our custom autoloader.
+    require Path::normalize(PATH_LIBRARY . '/FOSSBilling/Autoloader.php');
+    $loader = new FOSSBilling\AutoLoader();
+    $loader->register();
+
+    // Load the DI container.
+    global $di;
+    $di = require Path::normalize(PATH_ROOT . '/di.php');
+
+    // Now that the config file is loaded, we can enable Sentry.
+    SentryHelper::registerSentry();
+}
+
+/*
+ * Post-initialization.
+ */
+function postInit() : void
+{
+    // Set error and exception handlers, and default logging settings.
+    ini_set('log_errors', '1');
+    ini_set('html_errors', false);
+    ini_set('error_log', Path::normalize(PATH_LOG . '/php_error.log'));
+    error_reporting(E_ALL);
+
+    if (DEBUG) {
+        ini_set('display_errors', '1');
+        ini_set('display_startup_errors', '1');
+    } else {
+        ini_set('display_errors', '0');
+        ini_set('display_startup_errors', '0');
+    }
+}
+
+// Perform pre-initialization (loading required dependencies, etc.).
+preInit();
+
+// Initialize the application.
+init();
 
 // Check for legacy BoxBilling/FOSSBilling files.
 checkLegacyFiles();
-
-// Check config exists.
-checkConfig();
-
-require PATH_LIBRARY . DIRECTORY_SEPARATOR . 'FOSSBilling' . DIRECTORY_SEPARATOR . 'Config.php';
-
-// Set globals and relevant settings based on the config.
-date_default_timezone_set(Config::getProperty('i18n.timezone', 'UTC'));
-define('ADMIN_PREFIX', Config::getProperty('admin_area_prefix'));
-define('DEBUG', (bool) Config::getProperty('debug_and_monitoring.debug', false));
-define('PATH_DATA', Config::getProperty('path_data'));
-define('PATH_CACHE', PATH_DATA . DIRECTORY_SEPARATOR . 'cache');
-define('PATH_LOG', PATH_DATA . DIRECTORY_SEPARATOR . 'log');
-define('INSTANCE_ID', Config::getProperty('info.instance_id', 'Unknown'));
-
-// Initial setup and checks passed, now we setup our custom autoloader.
-require PATH_LIBRARY . DIRECTORY_SEPARATOR . 'FOSSBilling' . DIRECTORY_SEPARATOR . 'Autoloader.php';
-$loader = new FOSSBilling\AutoLoader();
-$loader->register();
-
-// Set the system URL
-$protocol = Config::getProperty('security.force_https', true) || Tools::isHTTPS() ? 'https://' : 'http://';
-define('SYSTEM_URL', $protocol . Config::getProperty('url'));
-
-define('BIND_TO', Tools::getDefaultInterface());
-
-// Now that the config file is loaded, we can enable Sentry
-SentryHelper::registerSentry();
 
 // Verify the installer was removed.
 checkInstaller();
@@ -274,16 +316,8 @@ checkSSL();
 // Check web server and web server settings.
 checkWebServer();
 
-// Set error and exception handlers, and default logging settings.
-ini_set('log_errors', '1');
-ini_set('html_errors', false);
-ini_set('error_log', PATH_LOG . DIRECTORY_SEPARATOR . 'php_error.log');
-error_reporting(E_ALL);
+// Check whether the patcher needs to be run.
+checkUpdatePatcher();
 
-if (DEBUG) {
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-} else {
-    ini_set('display_errors', '0');
-    ini_set('display_startup_errors', '0');
-}
+// Perform post-initialization (setting error handlers, etc).
+postInit();
