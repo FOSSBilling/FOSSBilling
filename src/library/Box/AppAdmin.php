96b16b13087e235bd9da83c231a53caa0ab99ffe
Adam Daley <admdly@users.noreply.github.com>
2025-08-04 21:07:22 +0100

    Refactor Twig environment config and loading

--- src/library/Box/AppAdmin.php
+++ src/library/Box/AppAdmin.php
@@ -9,15 +9,8 @@
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
  */
 
-use DebugBar\Bridge\Twig\NamespacedTwigProfileCollector;
 use FOSSBilling\Environment;
-use FOSSBilling\TwigExtensions\DebugBar;
 use Symfony\Component\Filesystem\Path;
-use FOSSBilling\Twig\DebugBarExtension;
-use Twig\Extension\AttributeExtension;
-use Twig\Extension\ProfilerExtension;
-use Twig\Profiler\Profile;
-use Twig\RuntimeLoader\FactoryRuntimeLoader;
 
 class Box_AppAdmin extends Box_App
 {
@@ -58,45 +51,13 @@ class Box_AppAdmin extends Box_App
         exit;
     }
 
+    /**
+     * Get Twig environment for admin area.
+     */
     protected function getTwig(): Twig\Environment
     {
-        $service = $this->di['mod_service']('theme');
-        $theme = $service->getCurrentAdminAreaTheme();
+        $twigFactory = new FOSSBilling\Twig\TwigFactory($this->di);
 
-        $loader = new Box_TwigLoader(
-            [
-                'mods' => PATH_MODS,
-                'theme' => Path::join(PATH_THEMES, $theme['code']),
-                'type' => 'admin',
-            ]
-        );
-
-        $twig = $this->di['twig'];
-        $twig->setLoader($loader);
-        $twig->addGlobal('theme', $theme);
-        $twig->addGlobal('current_theme', $theme['code']);
-        $twig->addGlobal('app_area', 'admin');
-
-        if (Environment::isDevelopment()) {
-            $profile = new Profile();
-            $twig->addExtension(new ProfilerExtension($profile));
-            $collector = new NamespacedTwigProfileCollector($profile);
-            if (!$this->debugBar->hasCollector($collector->getName())) {
-                $this->debugBar->addCollector($collector);
-            }
-        }
-
-        $twig->addExtension(new AttributeExtension(DebugBarExtension::class));
-        $twig->addRuntimeLoader(new FactoryRuntimeLoader([
-            DebugBarExtension::class => function () {
-                return new DebugBarExtension($this->getDebugBar());
-            },
-        ]));
-
-        if ($this->di['auth']->isAdminLoggedIn()) {
-            $twig->addGlobal('admin', $this->di['api_admin']);
-        }
-
-        return $twig;
+        return $twigFactory->createAdminEnvironment($this->debugBar);
     }
 }
--- src/library/FOSSBilling/Twig/Enum/AppArea.php
+++ src/library/FOSSBilling/Twig/Enum/AppArea.php
@@ -2,18 +2,17 @@
 
 declare(strict_types=1);
 /**
- * Copyright 2022-2026 FOSSBilling
+ * Copyright 2022-2025 FOSSBilling.
  * SPDX-License-Identifier: Apache-2.0.
  *
  * @copyright FOSSBilling (https://www.fossbilling.org)
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
  */
 
-namespace FOSSBilling\Enums;
+namespace FOSSBilling\Twig\Enum;
 
-enum SecurityCheckResultEnum: string
+enum AppArea: string
 {
-    case PASS = 'passed';
-    case WARN = 'warning';
-    case FAIL = 'failed';
+    case ADMIN = 'admin';
+    case CLIENT = 'client';
 }
