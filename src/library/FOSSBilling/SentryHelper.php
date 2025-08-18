a8a23f398403115b3bc41eb8b16940f4805dd427
Adam Daley <admdly@users.noreply.github.com>
2025-08-19 00:02:41 +0100

    Update file copyright header and add author tags

--- src/library/FOSSBilling/SentryHelper.php
+++ src/library/FOSSBilling/SentryHelper.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 /**
- * Copyright 2022-2025 FOSSBilling
- * Copyright 2011-2021 BoxBilling, Inc.
+ * Copyright 2022-2025 FOSSBilling.
  * SPDX-License-Identifier: Apache-2.0.
  *
+ * @author Belle Aerni
  * @copyright FOSSBilling (https://www.fossbilling.org)
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
  */
