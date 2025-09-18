c8ef175bdb24a8594b7a1bef62ec5141752b0e3e
Yağızhan <yagizhan49@protonmail.com>
2025-09-18 13:23:24 +0300

    Refactor IP retrieval method in Server.php

--- src/modules/Servicelicense/Server.php
+++ src/modules/Servicelicense/Server.php
@@ -49,24 +49,6 @@ class Server implements \FOSSBilling\InjectionAwareInterface
         return $_SERVER[$key] ?? $default;
     }
 
-    private function getIp($checkProxy = true)
-    {
-        $ip = null;
-        if ($checkProxy && $this->getServer('HTTP_CLIENT_IP') != null) {
-            $ip = $this->getServer('HTTP_CLIENT_IP');
-        } else {
-            if ($checkProxy && $this->getServer('HTTP_X_FORWARDED_FOR') != null) {
-                $ip = $this->getServer('HTTP_X_FORWARDED_FOR');
-            } else {
-                $ip = $this->getServer('REMOTE_ADDR');
-            }
-        }
-
-        $ips_arr = explode(',', $ip);
-
-        return trim($ips_arr[0]);
-    }
-
     public function process($data)
     {
         $data = is_array($data) ? $data : (json_decode($data ?? '', true) ?: []);
@@ -101,7 +83,7 @@ class Server implements \FOSSBilling\InjectionAwareInterface
             throw new \LogicException('Path key is not present in call', 1004);
         }
 
-        $ip = $this->getIp();
+        $ip = $this->di['request']->getClientIp();
         $host = $data['host'];
         $version = $data['version'];
         $path = $data['path'];
