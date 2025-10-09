f018efd863a51c0b332328fe4ab38bb964a7cfcc
Adam Daley <admdly@users.noreply.github.com>
2025-10-09 14:42:16 +0100

    Refactor DB config to use 'driver' instead of 'type'

--- src/library/FOSSBilling/Doctrine/DriverManagerFactory.php
+++ src/library/FOSSBilling/Doctrine/DriverManagerFactory.php
@@ -14,21 +14,50 @@ namespace FOSSBilling\Doctrine;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 use FOSSBilling\Config;
+use FOSSBilling\Exception;
 
 class DriverManagerFactory
 {
+    /**
+     * List of supported database drivers for Doctrine DBAL connections.
+     *
+     * @var string[]
+     */
+    const SUPPORTED_DRIVERS = [
+        'pdo_mysql',
+    ];
+
+    /**
+     * Creates and returns a Doctrine DBAL Connection instance.
+     *
+     * @param array $driverOptions Optional driver-specific options.
+     * @return Connection
+     * @throws Exception If required database configuration keys are missing or the driver is unsupported.
+     */
     public static function getConnection(array $driverOptions = []): Connection
     {
         $dbConfig = Config::getProperty('db');
+
+        $requiredKeys = ['driver', 'host', 'port', 'name', 'user', 'password'];
+        foreach ($requiredKeys as $key) {
+            if (!isset($dbConfig[$key])) {
+                throw new Exception('Database configuration missing required key: :key.', [':key' => $key]);
+            }
+        }
+
+        if (!in_array($dbConfig['driver'], self::SUPPORTED_DRIVERS, true)) {
+            throw new Exception('Unsupported database driver :driver. Supported drivers are: :supported.', [':driver' => $dbConfig['driver'], ':supported' => implode(', ', self::SUPPORTED_DRIVERS)]);
+        }
+
         $connectionParams = [
-            'driver'   => 'pdo_mysql',
+            'driver'   => $dbConfig['driver'],
             'host'     => $dbConfig['host'],
             'port'     => $dbConfig['port'],
             'dbname'   => $dbConfig['name'],
             'user'     => $dbConfig['user'],
             'password' => $dbConfig['password'],
             'driverOptions' => $driverOptions,
-            'charset'  => 'utf8',
+            'charset'  => $dbConfig['charset'] ?? 'utf8',
         ];
 
         return DriverManager::getConnection($connectionParams);
