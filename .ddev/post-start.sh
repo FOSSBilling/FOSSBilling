448ecc9efc882dc93d593c96fe2c94d9935d206f
Belle Aerni <belleaerni@gmail.com>
2025-10-14 05:23:22 -0700

    Changes based on copilot review

--- .ddev/post-start.sh
+++ .ddev/post-start.sh
@@ -2,13 +2,13 @@
 
 # NPM vars
 nodeModulesPath="node_modules"
-packageLock="package.lock"
-nodeBuildLock="node_modules/package.lock"
+packageLock="package-lock.json"
+nodeBuildTimestamp="node_modules/package.stamp"
 
 # Composer vars
 composerVendorPath="src/vendor"
 composerLock="composer.lock"
-customVendorLock="src/vendor/composer.lock" # Does this file for sure change each time the deps are updated?
+customVendorTimestamp="src/vendor/composer.stamp" # Does this file for sure change each time the deps are updated?
 
 # Other vars
 npmBuildNeeded=0
@@ -19,7 +19,7 @@ echo -e "$infoString Automatically installing / updating dependencies and buildi
 sleep 2
 
 # If the NPM packages aren't installed or are outdated, install the locked versions
-if [ ! -d "$nodeModulesPath" ] || [ ! -f "$nodeBuildLock" ] || [ "$packageLock" -nt "$nodeBuildLock" ]; then
+if [ ! -d "$nodeModulesPath" ] || [ ! -f "$nodeBuildTimestamp" ] || [ "$packageLock" -nt "$nodeBuildTimestamp" ]; then
     npm install
     npmBuildNeeded=1
 elif [ ! -d "src/themes/admin_default/build" ]; then
@@ -27,15 +27,15 @@ elif [ ! -d "src/themes/admin_default/build" ]; then
 fi
 
 # If the composer packages aren't installed or are outdated, install the locked versions
-if [ ! -d "$composerVendorPath" ] || [ ! -f "$customVendorLock" ] || [ "$composerLock" -nt "$customVendorLock" ]; then
-    echo "$now" > $customVendorLock
+if [ ! -d "$composerVendorPath" ] || [ ! -f "$customVendorTimestamp" ] || [ "$composerLock" -nt "$customVendorTimestamp" ]; then
     composer install
+    echo "$now" > "$customVendorTimestamp"
 fi
 
 # If we changed NPM packages, then re-run the build automatically
 if [ $npmBuildNeeded -eq 1 ]; then
     npm run build
-    echo "$now" > $nodeBuildLock
+    echo "$now" > "$nodeBuildTimestamp"
 fi
 
 echo -e "$infoString Done!"
