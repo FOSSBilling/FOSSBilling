3f4af3bb320c579faf2f7c7389b8175d44ca934a
Iced-Coded <volkogon212@gmail.com>
2025-10-30 10:00:20 +0200

    Fix space consistency in Guest.php

--- src/modules/Spamchecker/Api/Guest.php
+++ src/modules/Spamchecker/Api/Guest.php
@@ -30,7 +30,7 @@ class Guest extends \Api_Abstract
             'version' => $config['captcha_version'] ?? null,
             'captcha_provider' => $config['captcha_provider'] ?? 'recaptcha_v2',
             'turnstile_site_key' => $config['turnstile_site_key'] ?? null,
-            'hcaptcha_site_key'=> $config['hcaptcha_site_key'] ?? null,
+            'hcaptcha_site_key' => $config['hcaptcha_site_key'] ?? null,
         ];
     }
 }
