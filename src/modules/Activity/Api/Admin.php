cfb7ca2f5bed2a641eff7e17819ebfc2185e12ba
Yağızhan Burak Yakar <yagizhan49@protonmail.com>
2025-12-23 15:15:23 +0300

    These were removed in #3026 and seem to be accidentally reintroduced.

--- src/modules/Activity/Api/Admin.php
+++ src/modules/Activity/Api/Admin.php
@@ -98,40 +98,4 @@ class Admin extends \Api_Abstract
 
         return $this->getService()->logEmail($subject, $client_id, $sender, $recipients, $content_html, $content_text);
     }
-
-    /**
-     * Remove an activity message from the log.
-     *
-     * @param array $data Message data
-     *
-     * @return bool True if the message was deleted, false otherwise
-     */
-    #[RequiredParams(['id' => 'ID was not passed'])]
-    public function log_delete($data)
-    {
-        $model = $this->di['db']->getExistingModelById('ActivitySystem', $data['id'], 'Event not found');
-
-        $this->di['mod_service']('Staff')->checkPermissionsAndThrowException('activity', 'delete_activity');
-
-        $this->di['db']->trash($model);
-
-        return true;
-    }
-
-    /**
-     * Delete multiple activity messages from the log.
-     *
-     * @param array $data Deletion data
-     *
-     * @return bool True if the messages were deleted, false otherwise
-     */
-    #[RequiredParams(['ids' => 'IDs were not passed'])]
-    public function batch_delete($data)
-    {
-        foreach ($data['ids'] as $id) {
-            $this->log_delete(['id' => $id]);
-        }
-
-        return true;
-    }
 }
