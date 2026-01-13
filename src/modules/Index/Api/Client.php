127a5082ea066fa8f7401b9823b57f0c2f73cf90
Adam Daley <admdly@users.noreply.github.com>
2026-01-13 13:48:30 +0000

    Add Client Dashboard API endpoint
    Add new consolidated API endpoint for the client dashboard that reduces the number of individual API calls needed to render the dashboard.

---
+++ src/modules/Index/Api/Client.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Copyright 2022-2025 FOSSBilling
+ * SPDX-License-Identifier: Apache-2.0.
+ *
+ * @copyright FOSSBilling (https://www.fossbilling.org)
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
+ */
+
+namespace Box\Mod\Index\Api;
+
+class Client extends \Api_Abstract
+{
+    public function get_dashboard($data): array
+    {
+        $identity = $this->getIdentity();
+
+        return $this->getService()->getDashboardData($identity);
+    }
+}
