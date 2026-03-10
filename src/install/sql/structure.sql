f3dbb32c8fb5d8203095a8eec98b3bf11c30456e
Adam Daley <admdly@users.noreply.github.com>
2026-03-10 08:28:27 +0000

    Update SQL dump header database name

--- src/install/sql/structure.sql
+++ src/install/sql/structure.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0, for Linux (x86_64)
 --
--- Host: localhost    Database: boxbilling
+-- Host: localhost    Database: fossbilling
 -- ------------------------------------------------------
 -- Server version	8.0.x
 
