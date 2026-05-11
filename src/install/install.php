8e3ca3ddcd007829d1ea881d3f3f88beacce2598
Adam Daley <admdly@users.noreply.github.com>
2026-05-11 16:45:12 +0100

    Use config debug flag; always warn if install exists

--- src/install/install.php
+++ src/install/install.php
@@ -113,11 +113,9 @@ final class FOSSBilling_Installer
         require_once 'session.php';
         $this->session = new Session();
         $this->filesystem = new Filesystem();
-        $this->isDebug = !Environment::isProduction();
-
-        if (!$this->isDebug && $this->filesystem->exists(PATH_CONFIG)) {
+        if ($this->filesystem->exists(PATH_CONFIG)) {
             $config = require PATH_CONFIG;
-            $this->isDebug = $config['debug_and_monitoring']['debug'] || !Environment::isProduction();
+            $this->isDebug = (bool) ($config['debug_and_monitoring']['debug'] ?? false);
         }
 
         if (getenv('IS_DDEV') === 'true' && ($_GET['a'] ?? 'index') === 'index') {
