a970ca3518a759b6987fb6672d75cf8fe2f71c01
Adam Daley <admdly@users.noreply.github.com>
2026-05-22 15:37:05 +0100

    Use Locales::getNames; tweak language selector CSS

--- src/library/FOSSBilling/i18n.php
+++ src/library/FOSSBilling/i18n.php
@@ -14,7 +14,7 @@ namespace FOSSBilling;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Path;
 use Symfony\Component\Finder\Finder;
-use Symfony\Component\Intl\Languages;
+use Symfony\Component\Intl\Locales;
 
 class i18n
 {
@@ -132,10 +132,10 @@ class i18n
 
         // Handle when FOSSBilling is running with a dummy locale folder.
         $localePhpPath = Path::join(PATH_LANGS, 'locales.php');
-        $array = ($filesystem->exists($localePhpPath)) ? include $localePhpPath : Languages::getNames(self::getActiveLocale(false));
+        $array = ($filesystem->exists($localePhpPath)) ? include $localePhpPath : Locales::getNames(self::getActiveLocale(false));
 
         foreach ($locales as $locale) {
-            $title = ($array[$locale] ?? $locale) . " ($locale)";
+            $title = $array[$locale] ?? $locale;
             $details[] = [
                 'locale' => $locale,
                 'title' => $title,
