861d1d2dd880e51b1bdd66f15885e9e0cf8bb888
Adam Daley <admdly@users.noreply.github.com>
2026-05-28 06:45:47 +0100

    Replace DBAL with PDO in UpdatePatcher

--- tests-legacy/library/FOSSBilling/UpdatePatcherTest.php
+++ tests-legacy/library/FOSSBilling/UpdatePatcherTest.php
@@ -2,35 +2,35 @@
 
 declare(strict_types=1);
 
-use Doctrine\DBAL\Connection;
 use FOSSBilling\UpdatePatcher;
 use PHPUnit\Framework\Attributes\Group;
 
 #[Group('Core')]
 final class UpdatePatcherTest extends PHPUnit\Framework\TestCase
 {
-    public function testSetDiRegistersDbalWhenUpdatingWithLegacyContainer(): void
+    public function testSetDiDoesNotRequireDbalWhenUpdatingWithLegacyContainer(): void
     {
         $di = new Pimple\Container();
 
         $patcher = new UpdatePatcher();
         $patcher->setDi($di);
 
-        self::assertTrue($di->offsetExists('dbal'));
+        self::assertFalse($di->offsetExists('dbal'));
     }
 
-    public function testDbalAccessCreatesConnectionWhenLegacyContainerWasInjectedBeforehand(): void
+    public function testDatabaseAccessUsesPdoWhenLegacyContainerWasInjectedBeforehand(): void
     {
         $di = new Pimple\Container();
+        $di['pdo'] = new PDO('sqlite::memory:');
         $patcher = new UpdatePatcher();
 
         $diProperty = new ReflectionProperty(UpdatePatcher::class, 'di');
         $diProperty->setValue($patcher, $di);
 
-        $method = new ReflectionMethod(UpdatePatcher::class, 'getDbalConnection');
-        $connection = $method->invoke($patcher);
+        $method = new ReflectionMethod(UpdatePatcher::class, 'getPdo');
+        $pdo = $method->invoke($patcher);
 
-        self::assertInstanceOf(Connection::class, $connection);
+        self::assertSame($di['pdo'], $pdo);
         self::assertFalse($di->offsetExists('dbal'));
     }
 }
