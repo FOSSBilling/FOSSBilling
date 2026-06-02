b3a4c92a11024e594a31112e4718e33d882e5dfe
Adam Daley <admdly@users.noreply.github.com>
2026-06-02 04:14:05 +0100

    Ignore properties absent in both fingerprints

--- src/library/FOSSBilling/Fingerprint.php
+++ src/library/FOSSBilling/Fingerprint.php
@@ -126,6 +126,8 @@ class Fingerprint
                 ++$itemCount;
                 $scoreSubtract += $properties['weight'];
                 $differing[] = $name;
+            } elseif (!$exitsInFingerprint && !$exitsInCurrentFingerprint) {
+                // Do nothing in this case, as the property isn't in either fingerprint.
             } else {
                 ++$itemCount;
                 $hashedData = hash('md5', (string) $properties['source']);
