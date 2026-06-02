31433ea5464b6028286d6345314a7a39c48ae806
Adam Daley <admdly@users.noreply.github.com>
2026-06-02 04:14:48 +0100

    Remove public_ticket_create API method

--- src/modules/Support/Api/Admin.php
+++ src/modules/Support/Api/Admin.php
@@ -285,17 +285,6 @@ class Admin extends \Api_Abstract
     }
 
     /**
-     * Create new public inquiry.
-     *
-     * @throws \FOSSBilling\Exception
-     */
-    #[RequiredParams(['name' => 'Name is required', 'email' => 'Email is required', 'subject' => 'Subject is required', 'message' => 'Message is required'])]
-    public function public_ticket_create(array $data): int
-    {
-        return $this->getService()->publicTicketCreate($data, $this->getIdentity());
-    }
-
-    /**
      * Set id status to closed.
      *
      * @throws \FOSSBilling\Exception
@@ -575,17 +564,6 @@ class Admin extends \Api_Abstract
         $model = $this->di['db']->getExistingModelById('SupportPrCategory', $data['id'], 'Canned category not found');
 
         return $this->getService()->cannedCategoryToApiArray($model);
-    }
-
-    /**
-     * Create new canned response category.
-     *
-     * @throws \FOSSBilling\Exception
-     */
-    #[RequiredParams(['title' => 'Category title is required'])]
-    public function canned_category_create(array $data): int
-    {
-        return $this->getService()->cannedCategoryCreate($data['title']);
     }
 
     /**
