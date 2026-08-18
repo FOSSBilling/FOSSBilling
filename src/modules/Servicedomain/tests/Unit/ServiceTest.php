2902fccb6ef99007dda3c05bc205145702e61d00
Adam Daley <admdly@users.noreply.github.com>
2026-08-18 14:23:13 +0100

    🐛 Restore Pest callback compatibility

--- src/modules/Servicedomain/tests/Unit/ServiceTest.php
+++ src/modules/Servicedomain/tests/Unit/ServiceTest.php
@@ -1136,7 +1136,7 @@ test('converts to api array', function (?Box\Mod\Staff\Entity\Admin $identity, s
         expect($result['registrar'])->toBe($tldRegistrar->getName());
     }
 })->with([
-    [\Tests\Helpers\admin(...), 'atLeast'],
+    [fn () => \Tests\Helpers\admin(), 'atLeast'],
     [null, 'never'],
 ]);
 
